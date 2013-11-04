ddfee3a96679d90762c2baf42648cf5bcd026028
Vincenzo Parisi <vincenzo.parisi@ntb.ch>
2013-11-04 15:21:43 +0000

    added comments

--- includes/eeros/sequencer/Sequence.hpp
+++ includes/eeros/sequencer/Sequence.hpp
@@ -14,21 +14,81 @@ namespace eeros{
 
 		enum { kSequenceRunning = 0, kSequenceFinished = 1, kSequenceNotStarted = 2};
 
-				
+		/**
+		 *Usage: 
+		 * The first allocation of a sequencer is automatically the Main-Sequencer!!
+		 * MySequencer mainSequencer("MainSequencer");
+		 * MySequence sequence("MySequence", mainSequencer);
+		 */		
 		class Sequence : public Runnable
 		{
 		public:
+			/**
+			 * Defining pointer to a methode as a type
+			 */
 			typedef void (Sequence::*method)();
+			/**
+			 * Constructor. name is the name of the sequence, this name have toi be uniqu, else the constructoor will throw an exception 
+			 * and the caller is the sequencer, which will execute this sequence.
+			 * The state of the sequence is initialised with kSequenceNotStarted. 
+			 */
 			Sequence(std::string name, Sequencer& caller);
+			
+			/**
+			 * Destructor removes this sequence from the list allSequences. 
+			 */
 			~Sequence();
+			
+			/**
+			 * returns the name of the sequence
+			 */
 			std::string getName();
+			
+			/** add a callback (own type method) to the list which will be called in teh run() method from the callerThread.
+			  * Make sure, that the thread will be only as "one shot"
+			  */
 			void addCallBack(method callback);
+			
+			/**
+			 * Finds a callback in the list callBacks and with setCurrent, you can set the iterator to it.
+			 * This is useful in exceptions, if you want to recall a method.
+			 */
 			std::list<Sequence::method>::iterator findCallBack(method callback, bool setCurrent);
+			
+			/**
+			 * Helper for findCallBack, it sets the current iterator in th callBacks list.
+			 */
 			void setCurrentCallBack(std::list<Sequence::method>::iterator iter);
+			
+			/**
+			 * The run method will be called through the thread (sequencer, callerThread) and normally it will execute every method in the list callBacks.
+			 */
 			virtual void run();
+			
+			/**
+			 * fillCallBacks() have to be implemented by the user, in this method you can fill the callBacks list with the desired method in the desired sequence to be executed.
+			 * Referto the collaboration wiki or the documention for further details.
+			 */
 			virtual void fillCallBacks() = 0;
+			
+			/**
+			 * Every Sequence is saved on cration in the list allSequences, so you can reuse it.
+			 * Since the sequence is identified by name, it can be used to get it.
+			 */
 			static Sequence* getSequence(std::string name);
+			
+			/** 
+			 * removes the sequence with name from the list allSequences. 
+			 * this is used in the destructor
+			 */
 			static void removeSequence(std::string name);
+			
+			/**
+			 * There are three states 
+			 * kSequenceRunning: the sequence is running
+			 * kSequenceFinished: the sequence has finished the execution
+			 * kSequenceNotStarted: the sequence is not started
+			 */
 			int getState();
 		protected:
 			Sequencer& callerThread;
--- includes/eeros/sequencer/SequenceException.hpp
+++ includes/eeros/sequencer/SequenceException.hpp
@@ -9,12 +9,35 @@ namespace eeros{
 	namespace sequencer{
 		class ErrorHandler;
 
+		/**
+		 * This Exception can be thrown. 
+		 * You can define how the system have to continue the excecution in the sequence by creating a ErrorHandler.
+		 */
 		class SequenceException{
 			friend class Sequence;
 		public:
+			/**
+			 * Create an exception containing only a message string reason
+			 */
 			SequenceException(std::string reason);
+			
+			/**
+			 * Create an exception containing 
+			 * the sequence seqCause which caused the exception 
+			 * the sequence methode cause, which caused the exception 
+			 * the sequence methode next, where the execution should continue
+			 * the ErrorHandler error, which does some work before starting the next method.
+			 * By setting toBegin to true, the sequence will be restarted and the flag goToNext will be ignored
+			 * If toBegin is false, then you can define goToNext with true, so the sequence starts at the method next, 
+			 * it is very important, that this method is in the callback list.
+			 * message string reason
+			 */
 			SequenceException(Sequence* seqCause, Sequence::method cause, Sequence::method next,
 				              ErrorHandler* error, bool toBegin, bool goToNext, std::string reason);
+			
+			/**
+			 * The destructor deletes the ErrorHandler
+			 */
 			~SequenceException();
 		private:
 			Sequence* seqenceCausing;
