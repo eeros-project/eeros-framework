33aa1f2994e619133f0cd463fdda1e3a1a88dec3
Vincenzo Parisi <vincenzo.parisi@ntb.ch>
2013-11-04 15:19:45 +0000

    added comments

--- includes/eeros/sequencer/Sequencer.hpp
+++ includes/eeros/sequencer/Sequencer.hpp
@@ -13,20 +13,81 @@ namespace eeros{
 	};
 	
 	namespace sequencer{
+		
 		class SequenceException;
 
+		/**
+		 * Sequencer is a class, which runs as a thread and will invoke severals Sequencesto be run.
+		 * To use this class you have to build your own seqencer. 
+		 * Usage:
+		 * class MySequencer : public eeros::sequencer::Sequencer{
+		 * public:
+		 *	MySequencer(std::string name);
+		 * };
+		 * 
+		 * in the main function:
+		 * The first allocation of a sequencer is automatically the Main-Sequencer!!
+		 * MySequencer mainSequencer("MainSequencer");
+		 * MySequence sequence("MySequence", mainSequencer);
+
+		 * create the thread
+		 * mainSequencer.start();
+		 *
+		 * Remark:
+		 * The sequence should not be called periodically, the thread should only exisat until the sequence has terminated (one shot)
+		 */
 		class Sequencer : public Executor
 		{
 		public:
+			/*
+			 * In a system you have one main sequencer and many sub sequncers, the sub sequencers are filled in a List.
+			 * getMainSequencer returns the main one.
+			 */
 			static Sequencer* getMainSequencer();
+			
+			/*
+			 * Create an object by initialising the Executor with period null and the first call will generate the main sequencer.
+			 */
 			Sequencer(std::string name);
+			
+			/*
+			 * does nothing
+			 */
 			virtual ~Sequencer();
+			
+			/*
+			 * Adds a time domain to the time domain list timeDomains
+			 */
+			//void addTimeDomain(TimeDomain* tDomain);
 			void addTimeDomain(control::TimeDomain* tDomain);
+			
+			/*
+			 * deltes all time domains from the list timeDomains
+			 */
 			void deleteAllTimeDomains();
+			
+			/*
+			 * Fills the sub sequencer in the subSequencers List
+			 */
 			void addSubSequencer(Sequencer* seq);
+			
+			/*
+			 * Clears the list of sub sequencer
+			 */
 			void deleteAllSubSequencers();
+			
+			/**
+			 * returns the name of the sequence
+			 */
 			std::string getName();
+			
+			/*
+			 * Find a sub sequencer by name, every Sequencer is identified by name.
+			 */
 			Sequencer* findSequencer(std::string name);
+			/*
+			 * Deletes a sub sequencer by name.
+			 */
 			void deleteSequencer(std::string name);
 		protected:
 			
