e9667a7a4687a634a926e3ec471b868f02779732
Martin Zueger <martin@mzueger.ch>
2014-04-09 13:13:44 +0200

    classes for frame and coordinate system added (untested)

---
+++ src/math/CoordinateSystem.cpp
@@ -0,0 +1,17 @@
+#include <eeros/math/Frame.hpp>
+#include <eeros/core/EEROSException.hpp>
+
+using namespace eeros;
+using namespace eeros::math;
+
+CoordinateSystem::CoordinateSystem(std::string id) : id(id) {
+	// nothing to do
+}
+
+bool CoordinateSystem::operator==(const CoordinateSystem& right) const {
+	return this == &right;
+}
+
+bool CoordinateSystem::operator!=(const CoordinateSystem& right) const {
+	return this != &right;
+}
---
+++ src/math/Frame.cpp
@@ -0,0 +1,34 @@
+#include <eeros/math/Frame.hpp>
+#include <eeros/core/EEROSException.hpp>
+
+using namespace eeros;
+using namespace eeros::math;
+
+Frame::Frame(const CoordinateSystem& a, const CoordinateSystem& b) : a(a), b(b) {
+	T.eye();
+}
+
+Frame::Frame(const CoordinateSystem& a, const CoordinateSystem& b, eeros::math::Matrix<4, 4, double> T) : a(a), b(b), T(T) {
+	// nothing to do
+}
+
+Frame::Frame(const CoordinateSystem& a, const CoordinateSystem& b, eeros::math::Matrix<3, 3, double> R, eeros::math::Matrix<3, 1, double> r) : a(a), b(b) {
+	for(int n = 0; n < 3; n++) {
+		for(int m = 0; m < 3; m++) {
+			T(m, n) = R(m,n);
+		}
+	}
+	for(int m = 0; m < 3; m++) {
+		T(m, 3) = r(m);
+	}
+	T(3, 0) = 0; T(3, 1) = 0; T(3, 2) = 0;
+	T(3, 3) = 1;
+}
+
+Frame Frame::operator*(const Frame& right) const {
+	if(b != right.a) {
+		throw EEROSException("Frame coordinate systems incompatible!");
+	}
+	Frame result(a, right.b, T * right.T);
+	return result;
+}
