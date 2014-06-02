8a64152ff5a2d678333c739c9709690299b15609
Martin Zueger <martin@mzueger.ch>
2014-06-02 16:25:45 +0200

    matrix class: left hand scalar operations completed

--- includes/eeros/math/Matrix.hpp
+++ includes/eeros/math/Matrix.hpp
@@ -758,11 +758,44 @@ namespace eeros {
 		/********** Operators **********/
 		
 		template < uint8_t M, uint8_t N = 1, typename T = double >
+		Matrix<M, N, T> operator+(T left, Matrix<M, N, T> right) {
+			Matrix<M, N, T> result;
+			for(uint8_t m = 0; m < M; m++) {
+				for(uint8_t n = 0; n < N; n++) {
+					result(m, n) = left + right(m, n);
+				}
+			}
+			return result;
+		}
+		
+		template < uint8_t M, uint8_t N = 1, typename T = double >
+		Matrix<M, N, T> operator-(T left, Matrix<M, N, T> right) {
+			Matrix<M, N, T> result;
+			for(uint8_t m = 0; m < M; m++) {
+				for(uint8_t n = 0; n < N; n++) {
+					result(m, n) = left - right(m, n);
+				}
+			}
+			return result;
+		}
+		
+		template < uint8_t M, uint8_t N = 1, typename T = double >
 		Matrix<M, N, T> operator*(T left, Matrix<M, N, T> right) {
 			Matrix<M, N, T> result;
 			for(uint8_t m = 0; m < M; m++) {
 				for(uint8_t n = 0; n < N; n++) {
-					result(m, n) = right(m, n) * left;
+					result(m, n) = left * right(m, n);
+				}
+			}
+			return result;
+		}
+		
+		template < uint8_t M, uint8_t N = 1, typename T = double >
+		Matrix<M, N, T> operator/(T left, Matrix<M, N, T> right) {
+			Matrix<M, N, T> result;
+			for(uint8_t m = 0; m < M; m++) {
+				for(uint8_t n = 0; n < N; n++) {
+					result(m, n) = left / right(m, n);
 				}
 			}
 			return result;
