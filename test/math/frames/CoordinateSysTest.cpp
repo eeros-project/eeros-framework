429f95f28e7ebf7aed840bd63734bdfec0efd031
Martin Zueger <martin@mzueger.ch>
2014-06-12 10:08:37 +0200

    unit tests for coordinate system class completed

--- test/math/frames/CoordinateSysTest.cpp
+++ test/math/frames/CoordinateSysTest.cpp
@@ -10,10 +10,31 @@ int main(int argc, char *argv[]) {
 	
 	std::cout << "Testing class CoordinateSystem" << std::endl;
 	
+	/**************************************************/
+	
+	std::cout << "Test #" << testNo++ << ": Creation" << std::endl;
+	error = 0;
+	
 	CoordinateSystem a("a");
 	CoordinateSystem b("b");
+	bool e = false;
+	try {
+		CoordinateSystem b2("b");
+	}
+	catch(...) {
+		e = true;
+	}
+	if(!e) {
+		std::cout << "  -> Failure: creation of a second coordinate system with id 'b' should throw an exception!" << std::endl;
+		error++;
+	}
 	
-	std::cout << "Test #" << testNo++ << ": TODO" << std::endl;
+	errorSum += error;
+	std::cout << "    -> Test finished with " << error << " error(s)" << std::endl;
+	
+	/**************************************************/
+	
+	std::cout << "Test #" << testNo++ << ": Comparision" << std::endl;
 	error = 0;
 	
 	if(a != a) {
@@ -29,7 +50,28 @@ int main(int argc, char *argv[]) {
 	errorSum += error;
 	std::cout << "    -> Test finished with " << error << " error(s)" << std::endl;
 	
-	// ********** END **********
+	/**************************************************/
+	
+	std::cout << "Test #" << testNo++ << ": get by id" << std::endl;
+	error = 0;
+	
+	CoordinateSystem* ap = CoordinateSystem::getCoordinateSystem("a");
+	CoordinateSystem* bp = CoordinateSystem::getCoordinateSystem("b");
+	
+	if(ap != &a) {
+		std::cout << "  -> Failure: getting the coordinate system by id ('a') failed" << std::endl;
+		error++;
+	}
+	
+	if(bp != &b) {
+		std::cout << "  -> Failure: getting the coordinate system by id ('b') failed" << std::endl;
+		error++;
+	}
+	
+	errorSum += error;
+	std::cout << "    -> Test finished with " << error << " error(s)" << std::endl;
+	
+	/**************************************************/
 	
 	if(errorSum == 0) {
 		std::cout << "Matrix element access test succeeded" << std::endl;
