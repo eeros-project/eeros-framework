fcb4cfa7a781527d45d6c1a62abd1ece7ba1448c
Martin Zueger <martin@mzueger.ch>
2014-08-25 17:29:27 +0200

    trajectory generator base template added

---
+++ includes/eeros/control/TrajectoryGenerator.hpp
@@ -0,0 +1,45 @@
+#ifndef ORG_EEROS_CONTROL_TRAJECTORYGENERATOR_HPP_
+#define ORG_EEROS_CONTROL_TRAJECTORYGENERATOR_HPP_
+
+#include <array>
+
+namespace eeros {
+	namespace control {
+		
+		template<typename T, int N = 1>
+		class TrajectoryGenerator {
+			
+		public:
+			virtual bool finished() = 0;
+			
+			virtual std::array<T, N> get(double dt) = 0;
+			
+			virtual bool push(std::array<T, N> start, std::array<T, N> end) = 0;
+			
+			virtual bool push(T end) {
+				std::array<T, N> e;
+				for(auto& i : e) i = 0;
+				e[0] = end;
+				return push(last, e);
+			}
+			
+			virtual bool push(std::array<T, N> end) {
+				return push(last, end);
+			}
+			
+			virtual bool push(T start, T end) {
+				std::array<T, N> s, e;
+				for(auto& i : e) i = 0; for(auto& i : s) i = 0;
+				s[0] = start; e[0] = end;
+				return push(s, e);
+			}
+			
+		protected:
+			std::array<T, N> last;
+		};
+
+	};
+};
+
+#endif /* ORG_EEROS_CONTROL_TRAJECTORYGENERATOR_HPP_ */
+
