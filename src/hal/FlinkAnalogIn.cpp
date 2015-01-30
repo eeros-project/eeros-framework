d75cead45244d48c3e11906980aee8571bd21b0e
Marco Tinner <marco.tinner@ntb.ch>
2015-01-30 14:37:43 +0100

    structure code a little bit better

--- src/hal/FlinkAnalogIn.cpp
+++ src/hal/FlinkAnalogIn.cpp
@@ -4,11 +4,10 @@ using namespace eeros::hal;
 
 FlinkAnalogIn::FlinkAnalogIn(std::string id, FlinkDevice* device, uint8_t subDeviceNumber, uint32_t channel, double scale, double offset, bool twosComplement) : ScalablePeripheralInput<double>(id, 1, 0), channel(channel), bitMask(0) {
 	this->subdeviceHandle = flink_get_subdevice_by_id(device->getDeviceHandle(), subDeviceNumber);
-	
+
 	flink_analog_in_get_resolution(subdeviceHandle, &resolution);
 	bitMask = (1 << resolution) - 1;
 	
-
 	this->scale = scale;
 	this->offset = offset;
 	this->twosComplement = twosComplement;
@@ -18,13 +17,9 @@ double FlinkAnalogIn::get() {
 	uint32_t data;
 	flink_analog_in_get_value(subdeviceHandle, channel, &data);
 	data &= bitMask;
-	if(twosComplement){
-		if((data >> (resolution - 1)) >= 1){ //negative number
-			int32_t complementsData = -((~data&bitMask) + 1); //invert bitwise and add one
-			return complementsData  * scale / bitMask + offset;
-		}else{ //positiv number
-			return data * scale / bitMask + offset;
-		}	
+	if(twosComplement && (data >> (resolution - 1)) >= 1){ //negative number
+		int32_t complementsData = -((~data&bitMask) + 1); //invert bitwise and add one
+		return complementsData  * scale / bitMask + offset;	
 	}else{
 		return data * scale / bitMask + offset;
 	}
