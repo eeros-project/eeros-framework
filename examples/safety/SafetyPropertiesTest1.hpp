ee5c7b01cb10c4d3fd2a1792ffd676f00a254011
Urs Graf <urs.graf@ntb.ch>
2016-11-03 15:53:17 +0100

    safety system improved, events now in own class

---
+++ examples/safety/SafetyPropertiesTest1.hpp
@@ -0,0 +1,106 @@
+#ifndef SAFETYPROPERTIESTEST1_HPP
+#define SAFETYPROPERTIESTEST1_HPP
+
+#include <eeros/safety/SafetyProperties.hpp>
+#include <eeros/hal/PeripheralOutput.hpp>
+#include <eeros/hal/PeripheralInput.hpp>
+#include <eeros/safety/InputAction.hpp>
+#include <eeros/safety/inputActions.hpp>
+#include <eeros/safety/OutputAction.hpp>
+#include <eeros/safety/SafetySystem.hpp>
+#include <eeros/core/Executor.hpp>
+	
+using namespace eeros;
+using namespace eeros::hal;
+using namespace eeros::safety;
+
+extern volatile bool running;
+
+class SafetyPropertiesTest1 : public SafetyProperties {
+public:
+	SafetyPropertiesTest1()  : 
+		seStartInitializing("start initializing"),
+		seInitializingDone("initialization done"),
+		seStartRunning("start running"),
+		seShutDown("start shuting down"),
+		seStopRunning("stop running"),
+		seSwitchingOff("switching off"),
+		slOff("off"),
+		slShutingDown("shuting down"),
+		slIinitializing("initializing"),
+		slInitialized("initialized"),
+		slRunning("running")
+		{	
+
+		HAL& hal = HAL::instance();
+
+		// ############ Define critical outputs ############
+		out1 = hal.getLogicPeripheralOutput("out1");
+		criticalOutputs = { out1 };
+
+		// ############ Define critical inputs ############
+		in1 = hal.getLogicPeripheralInput("in1");
+		criticalInputs = { in1 };
+		
+		// ############ Add levels ############
+		addLevel(slOff);
+		addLevel(slShutingDown);
+		addLevel(slIinitializing);
+		addLevel(slInitialized);
+		addLevel(slRunning);
+		
+		// ############ Add events to the levels ############
+		slOff.addEvent(seStartInitializing, slIinitializing, kPublicEvent);
+		slShutingDown.addEvent(seSwitchingOff, slOff, kPrivateEvent);
+		slIinitializing.addEvent(seInitializingDone, slInitialized, kPublicEvent);
+		slInitialized.addEvent(seStartRunning, slRunning, kPublicEvent);
+		slRunning.addEvent(seStopRunning, slInitialized, kPrivateEvent);
+		addEventToLevelAndAbove(slIinitializing, seShutDown, slShutingDown, kPublicEvent);
+
+		// ############ Define input states and events for all levels ############
+		slOff.setInputActions({ ignore(in1) });
+		slShutingDown.setInputActions({ ignore(in1) });
+		slIinitializing.setInputActions({ ignore(in1) });
+		slInitialized.setInputActions({ check(in1, true, seStartRunning) });
+		slRunning.setInputActions({ check(in1, false, seStopRunning) });
+
+		// ############ Define output states and events for all levels ############
+		slOff.setOutputActions({ set(out1, false) });
+		slShutingDown.setOutputActions({ set(out1, false) });
+		slIinitializing.setOutputActions({ set(out1, false) });
+		slInitialized.setOutputActions({ set(out1, false) });
+		slRunning.setOutputActions({ set(out1, true) });
+
+		// Define and add level functions
+		slOff.setLevelAction([&](SafetyContext* privateContext) {Executor::stop();});
+		slShutingDown.setLevelAction([&](SafetyContext* privateContext) {privateContext->triggerEvent(seSwitchingOff);});
+
+		// Define entry level
+		setEntryLevel(slOff);
+		
+		// Define action when exiting application with Ctrl-C 
+		exitFunction = [&](SafetyContext* privateContext) {privateContext->triggerEvent(seShutDown);};
+
+	}
+	
+	// critical outputs
+	PeripheralOutput<bool>* out1;
+	
+	// critical inputs
+	PeripheralInput<bool>* in1;
+	
+	SafetyEvent seStartInitializing;
+	SafetyEvent seInitializingDone;
+	SafetyEvent seStartRunning;
+	SafetyEvent seShutDown;
+	SafetyEvent seStopRunning;
+	SafetyEvent seSwitchingOff;
+	
+	SafetyLevel slOff;
+	SafetyLevel slShutingDown;
+	SafetyLevel slIinitializing;
+	SafetyLevel slInitialized;
+	SafetyLevel slRunning;
+};
+
+#endif // SAFETYPROPERTIESTEST1_HPP
---
+++ examples/safety/SafetySystemTest1.cpp
@@ -0,0 +1,68 @@
+//#include <initializer_list>
+#include <eeros/safety/SafetySystem.hpp>
+#include <eeros/hal/HAL.hpp>
+#include <eeros/core/EEROSException.hpp>
+#include <eeros/core/Executor.hpp>
+#include <eeros/logger/StreamLogWriter.hpp>
+#include <eeros/logger/Logger.hpp>
+#include <eeros/task/Lambda.hpp>
+#include <eeros/hal/DummyLogicInput.hpp>
+#include <eeros/hal/DummyLogicOutput.hpp>
+#include <unistd.h>
+#include <iostream>
+#include <signal.h>
+
+#include "SafetyPropertiesTest1.hpp"
+
+using namespace eeros;
+using namespace eeros::hal;
+using namespace eeros::safety;
+using namespace eeros::logger;
+
+void signalHandler(int signum) {
+	SafetySystem::exitHandler();
+}
+
+int main() {
+	signal(SIGHUP, signalHandler);
+	signal(SIGINT, signalHandler);
+	signal(SIGQUIT, signalHandler);
+	signal(SIGKILL, signalHandler);
+	signal(SIGTERM, signalHandler);
+	signal(SIGPWR, signalHandler);
+	
+	StreamLogWriter w(std::cout);
+	Logger<LogWriter>::setDefaultWriter(&w);
+	
+	Logger<LogWriter> log;
+	
+	log.info() << "Safety System Example started...";
+	
+	// Get HAL instance and initialize
+	HAL& hal = HAL::instance();
+	hal.addPeripheralInput(new DummyLogicInput("in1"));
+	hal.addPeripheralOutput(new DummyLogicOutput("out1"));
+	
+	// Create and initialize safety system
+	double period = 1;
+	SafetyPropertiesTest1 ssProperties;
+	SafetySystem safetySys(ssProperties, period);
+
+	safetySys.triggerEvent(ssProperties.seStartInitializing);
+
+	// Create and run executor
+	auto executor = eeros::Executor::instance();
+	executor.setPeriod(period);
+	executor.setMainTask(safetySys);
+	
+	eeros::task::Lambda l1 ([&] () {
+		static int count = 0;
+		if (count++ == 5) safetySys.triggerEvent(ssProperties.seInitializingDone);
+	});
+	eeros::task::Periodic t1("t1", period, l1);
+	executor.add(t1);
+
+	executor.run();
+
+	log.info() << "Test finished...";
+}
