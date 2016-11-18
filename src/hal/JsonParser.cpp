43a512c89fe108b792f4560240b138a3347097d1
Andreas Kalberer <andreas.kalberer@ntb.ch>
2016-11-18 16:54:35 +0100

    load library only if not already loaded by other device

--- src/hal/JsonParser.cpp
+++ src/hal/JsonParser.cpp
@@ -38,25 +38,24 @@ void JsonParser::createHalObjects(std::map<std::string, void*> libHandles){
 				if(subO.key() == "library"){
 					std::cout << "\tload library for " << subO.string_value() << std::endl;
 					library = subO.string_value();
-					libHandles[library] = dlopen(library.c_str(), RTLD_NOW); // try to load the given library and add it to libHandles map
-					auto libIt = libHandles.find(library);	
-					if(libIt->second == nullptr){
-						libHandles.erase(libIt);
-						std::cout << "could not load library: " << dlerror() << std::endl;
-						throw new eeros::EEROSException("could not load library: " + subO.string_value());
-					}
-					else{
-						std::cout << "lib successfully loaded" << std::endl;
-					}
-										
-					if(subO.string_value() == "libflinkeeros.so"){
-						std::cout << "will load flinkeeros" << std::endl;
-						
+					auto libIt = libHandles.find(library);
+					// if library not already in map of opened libraries -> try to open it
+					if(libIt == libHandles.end()){
+						libHandles[library] = dlopen(library.c_str(), RTLD_NOW); // try to load the given library and add it to libHandles map
+						auto libIt = libHandles.find(library);	
+						if(libIt->second == nullptr){
+							libHandles.erase(libIt);
+							std::cout << "could not load library: " << dlerror() << std::endl;
+							throw new eeros::EEROSException("could not load library: " + subO.string_value());
+						}
+						else{
+							std::cout << "lib successfully loaded" << std::endl;
+						}
 					}
 				}
 				else if(subO.key() == "devHandle"){
 					devHandle = subO.string_value();
-					std::cout << "\tdev Handle is " << subO.string_value() << std::endl;					
+					std::cout << "\tdev Handle is " << subO.string_value() << std::endl;
 				}
 				else if(std::regex_match(subO.key(), sdRegex)){
 					std::cout << "\t" << subO.key() << std::endl;
@@ -80,36 +79,38 @@ void JsonParser::createHalObjects(std::map<std::string, void*> libHandles){
 							// check if device already exists and library is loaded?
 							
 							auto libIt = libHandles.find(library);
-							if(!library.empty() && (libIt != libHandles.end()) ){
-								if(!devHandle.empty()){
-									for(const auto &chanProp : chanObj){
-										if(chanProp.key() == "signalId"){
-											std::cout << "\t\t\tsignalId: " << chanProp.string_value() << std::endl;
-											//TODO create function which can create string createDigOut from type
-											//TODO: change when using HAL Object: only for test purposes
-											std::string subDevStr = subDevO.key();
-											int subDevNumber = std::stoi(subDevStr.substr(9, subDevStr.length()));
-											std::cout << "subDevNum: " << subDevNumber << std::endl;
-											
-											std::string chanStr = chanObj.key();
-											int channelNumber = std::stoi(chanStr.substr(7, chanStr.length()));
-											std::cout << "channelNum: " << channelNumber << std::endl;
-											//TODO: upper lines only temporary
-											std::string createStr = "create" + type;
-											void *createHandle = dlsym(libIt->second, createStr.c_str());
-											if(createHandle == nullptr){
-												std::cout << "could not find createMethod: " << dlerror() << std::endl;
-												throw new eeros::EEROSException("could not find method in dynamic library");
+							if(!library.empty()){
+								if(libIt != libHandles.end()){
+									if(!devHandle.empty()){
+										for(const auto &chanProp : chanObj){
+											if(chanProp.key() == "signalId"){
+												std::cout << "\t\t\tsignalId: " << chanProp.string_value() << std::endl;
+												//TODO create function which can create string createDigOut from type
+												//TODO: change when using HAL Object: only for test purposes
+												std::string subDevStr = subDevO.key();
+												int subDevNumber = std::stoi(subDevStr.substr(9, subDevStr.length()));
+												std::cout << "subDevNum: " << subDevNumber << std::endl;
+												
+												std::string chanStr = chanObj.key();
+												int channelNumber = std::stoi(chanStr.substr(7, chanStr.length()));
+												std::cout << "channelNum: " << channelNumber << std::endl;
+												//TODO: upper lines only temporary
+												std::string createStr = "create" + type;
+												void *createHandle = dlsym(libIt->second, createStr.c_str());
+												if(createHandle == nullptr){
+													std::cout << "could not find createMethod: " << dlerror() << std::endl;
+													throw new eeros::EEROSException("could not find method in dynamic library");
+												}
+												//TODO make generic for In and Outputs and for different number of parameters
+												PeripheralOutput<bool> *halObj = reinterpret_cast<PeripheralOutput<bool> *(*)(std::string, std::string, uint32_t, uint32_t)>(createHandle)(chanProp.string_value(), devHandle, subDevNumber, channelNumber);
+												hal.addPeripheralOutput(halObj);
 											}
-											//TODO make generic for In and Outputs and for different number of parameters
-											PeripheralOutput<bool> *halObj = reinterpret_cast<PeripheralOutput<bool> *(*)(std::string, std::string, uint32_t, uint32_t)>(createHandle)(chanProp.string_value(), devHandle, subDevNumber, channelNumber);
-											hal.addPeripheralOutput(halObj);
 										}
 									}
-								}
-								else{
-									std::cout << "no device handle defined for " << subDevParam << std::endl;
-									throw new eeros::EEROSException("no device handle defined for " + subDevParam);
+									else{
+										std::cout << "no device handle defined for " << subDevParam << std::endl;
+										throw new eeros::EEROSException("no device handle defined for " + subDevParam);
+									}
 								}
 							}
 							else{
