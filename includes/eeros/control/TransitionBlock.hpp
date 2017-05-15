c8b2abab2e72d0523a1879f803273144140dd019
Urs Graf <urs.graf@ntb.ch>
2017-05-15 17:54:34 +0200

    rework

--- includes/eeros/control/TransitionBlock.hpp
+++ includes/eeros/control/TransitionBlock.hpp
@@ -1,45 +1,61 @@
 #ifndef ORG_EEROS_CONTROL_TRANSITIONBLOCK_HPP_
 #define ORG_EEROS_CONTROL_TRANSITIONBLOCK_HPP_
 
-#include <string>
-#include <eeros/core/Runnable.hpp>
+#include <mutex>
+#include <eeros/control/Block1i.hpp>
+#include <eeros/control/Block1o.hpp>
 
 namespace eeros {
 	namespace control {
 		
-		template < typename TObject >
-		class TransitionAction : public Runnable {
+		template < typename T > class TransitionBlock;
+		
+		template < typename T = double >
+		class TransitionBlockIn : public Block1i<T> {
 		public:
-			using TMethod = void (TObject::*)();
+			TransitionBlockIn(TransitionBlock<T>* c) : container(c) { }
+			virtual ~TransitionBlockIn() { }
 			
-			TransitionAction(TObject *object, TMethod method) :
-				object(object), method(method) { }
-
-			virtual void run() { (object->*method)(); }
-		private:
-			TObject *object;
-			TMethod method;
+			virtual void run() { 
+				container->mtx.lock();
+				container->val = this->getIn().getSignal().getValue(); 
+				container->mtx.unlock();
+			}
+			
+		protected:
+			TransitionBlock<T>* container;
 		};
 		
-		class TransitionBlock {
+		template < typename T = double >
+		class TransitionBlockOut : public Block1o<T> {
 		public:
-			TransitionBlock();
+			TransitionBlockOut(TransitionBlock<T>* c) : container(c) { }
+			virtual ~TransitionBlockOut() { }
 			
-			virtual void setName(std::string name);
-			virtual std::string getName();
+			virtual void run() { 
+				container->mtx.lock();
+				this->getOut().getSignal().setValue(container->val);
+				container->mtx.unlock();
+			}
 			
-			virtual Runnable* getRunnableA();
-			virtual Runnable* getRunnableB();
-			
-			virtual void runA() = 0;
-			virtual void runB() = 0;
-			
-		private:
-			std::string name;
-			TransitionAction<TransitionBlock> runnableA;
-			TransitionAction<TransitionBlock> runnableB;
+		protected:
+			TransitionBlock<T>* container;
 		};
 
+		template < typename T = double >
+		class TransitionBlock {
+		friend class TransitionBlockIn<T>;
+		friend class TransitionBlockOut<T>;
+		public:
+			TransitionBlock() : inBlock(this), outBlock(this) { }
+			
+			TransitionBlockIn<T> inBlock;
+			TransitionBlockOut<T> outBlock;
+			
+		private: 
+			T val;
+			std::mutex mtx;
+		};
 	};
 };
 
