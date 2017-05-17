8f8d9daf24f2eb84c99df6121e1f68703fa0f94c
MarcelGehrig <marcel.gehrig@ntb.ch>
2017-05-17 14:55:52 +0200

    EEROS finds ROS autonomous. ROS is not required, if no ROS-functionality is used

---
+++ includes/eeros/logger/ROSLogWriter.hpp
@@ -0,0 +1,29 @@
+#ifndef ORG_EEROS_LOGGER_ROSLOGWRITER_HPP_
+#define ORG_EEROS_LOGGER_ROSLOGWRITER_HPP_
+
+//#include <eeros/logger/LogWriter.hpp>
+//#include <eeros/logger/Logger.hpp>
+
+namespace eeros {
+	namespace logger {
+//		class ROSLogWriter : public LogWriter {
+		class ROSLogWriter  {
+		public:
+			ROSLogWriter();
+			
+//			virtual void show(LogLevel level = LogLevel::TRACE);
+//			virtual void begin(std::ostringstream& os, LogLevel level, unsigned category);
+//			virtual void end(std::ostringstream& os);
+//			virtual void endl(std::ostringstream& os);
+						
+//		private:
+//			std::ostream& out;
+//			LogLevel visible_level;
+//			bool enabled;
+//			bool colored;
+		};
+				
+	}
+}
+
+#endif /* ORG_EEROS_LOGGER_ROSLOGWRITER_HPP_ */
