5b4e5214d6df6dc3bf7ac030ce6d4f33c2e72edb
ursgraf <urs.graf@ntb.ch>
2017-08-04 15:57:18 +0200

    change sequences

--- examples/simpleMotorController/MainSequence.hpp
+++ examples/simpleMotorController/MainSequence.hpp
@@ -2,21 +2,47 @@
 #define ORG_EEROS_EXAMPLES_SEQUENCER_MAINSEQUENCE_HPP_
 
 #include <eeros/sequencer/Sequence.hpp>
+#include <eeros/sequencer/Step.hpp>
 #include <eeros/safety/SafetySystem.hpp>
 #include "MySafetyProperties.hpp"
 #include "MyControlSystem.hpp"
+#include <unistd.h>
 
 using namespace eeros::sequencer;
 using namespace eeros::safety;
 using namespace eeros::logger;
 
+class Move : public Step {
+public:
+	Move(std::string name, Sequencer& sequencer, BaseSequence* caller, MyControlSystem& cs) : Step(name, sequencer, caller), cs(cs) { }
+	int operator() (double pos) {this->pos = pos; return Step::start();}
+	int action() {
+		cs.setpoint.setValue(pos);
+	}
+	double pos;
+	MyControlSystem& cs;
+};
+
 class MainSequence : public Sequence {
 public:
-	MainSequence(std::string name, Sequencer& sequencer, SafetySystem& safetySys, MySafetyProperties& safetyProp, MyControlSystem& controlSys, double angle);
-	int action();
+	MainSequence(std::string name, Sequencer& seq, SafetySystem& safetySys, MySafetyProperties& safetyProp, MyControlSystem& cs, double angle) : 
+					Sequence(name, seq), safetySys(safetySys), safetyProp(safetyProp), angle(angle), controlSys(cs), move("move", seq, this, cs) {
+		log.info() << "Sequence created: " << name;
+		setNonBlocking();
+	}
+	int action() {
+		while(safetySys.getCurrentLevel() < safetyProp.slMoving);
 	
+		angle = 0;
+		while (true) {
+			angle += 6.28 / 10;
+			move(angle);
+			sleep(1);
+			log.info() << "enc =  " << controlSys.enc.getOut().getSignal().getValue();
+		}
+	}
 private:
-	bool isTerminating();
+	Move move;
 	double angle;
 	SafetySystem& safetySys;
 	MyControlSystem& controlSys;
