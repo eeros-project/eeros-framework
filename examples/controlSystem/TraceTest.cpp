58b9cc080fa445165a36edb9257c85c8e24f0628
ursgraf <urs.graf@ntb.ch>
2018-05-25 09:49:26 +0200

    improve example

--- examples/controlSystem/TraceTest.cpp
+++ examples/controlSystem/TraceTest.cpp
@@ -50,11 +50,11 @@ public:
 		// Define and add level functions
 		slOff.setLevelAction([&](SafetyContext* privateContext) {Executor::stop();});
 		slRunning.setLevelAction([&](SafetyContext* privateContext) {
-			if (slRunning.getNofActivations() == 30) {// start after 3s
+			if (slRunning.getNofActivations() == (int)(3 / period)) {// start after 3s
 				cs.trace1.enable();
 				cs.trace2.enable();
 			}
-			if (slRunning.getNofActivations() == 300) {// write to log file after 30s
+			if (slRunning.getNofActivations() % (int)(30 / period) == 0) {// write to log file every 30s
 				new TraceWriter<Vector3>(cs.trace2, "/tmp/ctrlData.txt");
 			}
 		});
@@ -115,7 +115,7 @@ int main() {
 	timestamp_t* timeStampBuf = controlSystem.trace1.getTimestampTrace();
 	Vector3* buf1 = controlSystem.trace1.getTrace();
 	Vector3* buf2 = controlSystem.trace2.getTrace();
-	for (int i = 0; i < controlSystem.trace1.getSize(); i++) file << timeStampBuf[i] << " " << buf1[i] << " " << buf2[i].getRow(0) << std::endl;
+	for (int i = 0; i < controlSystem.trace1.getSize(); i++) file << timeStampBuf[i] << " " << buf1[i] << " " << buf2[i] << std::endl;
 	file.close();
 	uint64_t stop = eeros::System::getTimeNs();
 	log.error() << stop - start;
