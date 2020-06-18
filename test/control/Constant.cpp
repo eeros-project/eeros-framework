cd8ce3170cafc5ad7cc5c5e95748aa6fa99a100c
ursgraf <urs.graf@ntb.ch>
2020-06-18 20:23:33 +0200

    Add test case

--- test/control/Constant.cpp
+++ test/control/Constant.cpp
@@ -13,19 +13,25 @@ TEST(controlConstantTest, initialValue) {
 	Constant<int> c2;
 	Constant<Matrix<2,1,double>> c3;
 	Constant<Matrix<2,1,int>> c4;
+  Constant<> c5(3.5);
 		
+  // values are set by default values for signals
 	EXPECT_TRUE(std::isnan(c1.getOut().getSignal().getValue()));
 	EXPECT_EQ(c2.getOut().getSignal().getValue(), std::numeric_limits<int32_t>::min());
 	EXPECT_TRUE(std::isnan(c3.getOut().getSignal().getValue()[0]));
 	EXPECT_EQ(c4.getOut().getSignal().getValue()[0], std::numeric_limits<int32_t>::min());
 	EXPECT_EQ(c4.getOut().getSignal().getValue()[1], std::numeric_limits<int32_t>::min());
+  EXPECT_TRUE(std::isnan(c5.getOut().getSignal().getValue()));
 	c1.run();
 	c2.run();
 	c3.run();
 	c4.run();
+  c5.run();
+  // values are set by default values of constants
 	EXPECT_TRUE(std::isnan(c1.getOut().getSignal().getValue()));
 	EXPECT_EQ(c2.getOut().getSignal().getValue(), std::numeric_limits<int32_t>::min());
 	EXPECT_TRUE(std::isnan(c3.getOut().getSignal().getValue()[0]));
 	EXPECT_EQ(c4.getOut().getSignal().getValue()[0], std::numeric_limits<int32_t>::min());
 	EXPECT_EQ(c4.getOut().getSignal().getValue()[1], std::numeric_limits<int32_t>::min());
+  EXPECT_EQ(c5.getOut().getSignal().getValue(), 3.5);
 }
