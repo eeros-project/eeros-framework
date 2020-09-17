6c4bd275d4ec56307a26ee1fed74720ff1db7fbf
ursgraf <urs.graf@ntb.ch>
2020-09-17 15:20:18 +0200

    Add initial state for feedback signals

--- includes/eeros/control/I.hpp
+++ includes/eeros/control/I.hpp
@@ -2,93 +2,164 @@
 #define ORG_EEROS_CONTROL_I_HPP_
 
 #include <eeros/control/Block1i1o.hpp>
+#include <eeros/logger/Logger.hpp>
+#include <cmath>
 
 namespace eeros {
-	namespace control {
+namespace control {
 
-		template < typename T = double >
-		class I: public Block1i1o<T> {
-		public:
-			I() : first(true), enabled(false) { prev.clear(); clearLimits(); }
-			
-			virtual void run() {
-				if(first) {  // first run, set output to init condition
-					this->out.getSignal().setValue(this->prev.getValue());
-					this->out.getSignal().setTimestamp(this->in.getSignal().getTimestamp());
-					this->prev.setTimestamp(this->out.getSignal().getTimestamp());
-					first = false;
-				}
-				else {
-					double tin = this->in.getSignal().getTimestamp() / 1000000000.0;
-					double tprev = this->prev.getTimestamp() / 1000000000.0;
-					
-					double dt = (tin - tprev);
-					T valin = this->in.getSignal().getValue();
-					T valprev = this->prev.getValue();
-					T output;
-					if(enabled) {
-						T val = valprev + valin * dt;
-						if ((val < upperLimit) && (val > lowerLimit)) output = val; 
-						else output = valprev;
-					} else
-						output = valprev;
-					this->out.getSignal().setValue(output);
-					this->out.getSignal().setTimestamp(this->in.getSignal().getTimestamp());
-					this->prev = this->out.getSignal();
-				}
-			}
+/**
+ * An integrator block is used to integrate an input signal. 
+ * The block can be enabled or disabled. When disabled the block no longer 
+ * integrates and keeps its actual state. An integrator must have its
+ * initial state set to a defined level. Further, the block allows to
+ * set an upper and lower limit to allow for anti windup.
+ *
+ * @tparam T - output type (double - default type)
+ * @since v0.6
+ */
+template < typename T = double >
+class I: public Block1i1o<T> {
+ public:
+  /**
+   * Constructs an integrator instance.\n
+   */
+  I() : first(true), enabled(false) {prev.clear(); clearLimits(); }
+ 
+  /**
+   * Disabling use of copy constructor because the block should never be copied unintentionally.
+   */
+  I(const I& s) = delete; 
 
-			virtual void enable() {
-				this->enabled = true;
-			}
-			virtual void disable() {
-				this->enabled = false;
-			}
-			virtual void setInitCondition(T val) {
-				this->prev.setValue(val);
-			}
-			virtual void setLimit(T upper, T lower) {
-				this->upperLimit = upper;
-				this->lowerLimit = lower;
-				T val = prev.getValue();
-				if (val > upper) prev.setValue(upper);
-				if (val < lower) prev.setValue(lower);
-			}
-			
-		protected:
-			bool first;
-			bool enabled;
-			Signal<T> prev;
-			T upperLimit, lowerLimit;
-		private:
-			virtual void clearLimits() {
-				_clear<T>();
-			}
-			template <typename S> typename std::enable_if<std::is_integral<S>::value>::type _clear() {
-				upperLimit = std::numeric_limits<int32_t>::max();
-				lowerLimit = -upperLimit;
-			}
-			template <typename S> typename std::enable_if<std::is_floating_point<S>::value>::type _clear() {
-				upperLimit = std::numeric_limits<double>::max();
-				lowerLimit = -upperLimit;
-			}
-			template <typename S> typename std::enable_if<!std::is_arithmetic<S>::value && std::is_integral<typename S::value_type>::value>::type _clear() {
-				upperLimit.fill(std::numeric_limits<int32_t>::max());
-				lowerLimit = -upperLimit;
-			}
-			template <typename S> typename std::enable_if<   !std::is_arithmetic<S>::value && std::is_floating_point<typename S::value_type>::value>::type _clear() {
-				upperLimit.fill(std::numeric_limits<double>::max());
-				lowerLimit = -upperLimit;
-			}
+  /**
+   * Runs the integration algorithm if the block is enabled, else
+   * the current state is left unchanged.
+   *
+   * @see setInitCondition(T val)
+   * @see setLimit(T upper, T lower)
+   * @see enable()
+   * @see disable()
+   */
+  virtual void run() {
+    double tin = this->in.getSignal().getTimestamp() / 1000000000.0;
+    double tprev = this->prev.getTimestamp() / 1000000000.0;
+    
+    double dt;
+    if (first) {
+      dt = 0; 
+      first = false;
+    } else dt = (tin - tprev);
+    T valin = this->in.getSignal().getValue();
+    T valprev = this->prev.getValue();
+    T output;
+    if (enabled) {
+      T val = valprev + valin * dt;
+      if ((val < upperLimit) && (val > lowerLimit)) output = val; 
+      else output = valprev;
+    } else output = valprev;
+    this->out.getSignal().setValue(output);
+    this->out.getSignal().setTimestamp(this->in.getSignal().getTimestamp());
+    this->prev = this->out.getSignal();
+  }
 
-		};
-		
-		/********** Print functions **********/
-		template <typename T>
-		std::ostream& operator<<(std::ostream& os, I<T>& i) {
-			os << "Block integrator: '" << i.getName() << "'"; 
-            return os;
-		}
-	};
+  /**
+   * Enables the integrator.
+   *
+   * If enabled, run() will integrate.
+   *
+   * @see disable()
+   */
+  virtual void enable() {
+    this->enabled = true;
+  }
+
+  /**
+   * Disables the integrator.
+   *
+   * If disabled, run() will not integrate and keep the current state.
+   *
+   * @see enable()
+   */
+  virtual void disable() {
+    this->enabled = false;
+  }
+  
+  /**
+   * Set the initial state of the integrator from where the integrator
+   * will start integrating when enabled.
+   *
+   * @see enable()
+   * @param val - initial state
+   */
+  virtual void setInitCondition(T val) {
+    this->prev.setValue(val);
+  }
+  
+  /**
+   * Set the upper and lower limit of the integrator.
+   *
+   * The integrator will not integrate above the upper or below
+   * the lower limit but will stay at this limiting values.
+   *
+   * @param upper - upper limit
+   * @param lower - lower limit
+   */
+  virtual void setLimit(T upper, T lower) {
+    this->upperLimit = upper;
+    this->lowerLimit = lower;
+    T val = prev.getValue();
+    if (val > upper) prev.setValue(upper);
+    if (val < lower) prev.setValue(lower);
+  }
+
+  /*
+   * Friend operator overload to give the operator overload outside
+   * the class access to the private fields.
+   */
+  template<typename X>
+  friend std::ostream &operator<<(std::ostream &os, I<X> &i);
+
+ protected:
+   eeros::logger::Logger log;
+  bool first;
+  bool enabled;
+  Signal<T> prev;
+  T upperLimit, lowerLimit;
+ private:
+  virtual void clearLimits() {
+    _clear<T>();
+  }
+  template <typename S> typename std::enable_if<std::is_integral<S>::value>::type _clear() {
+    upperLimit = std::numeric_limits<int32_t>::max();
+    lowerLimit = -upperLimit;
+  }
+  template <typename S> typename std::enable_if<std::is_floating_point<S>::value>::type _clear() {
+    upperLimit = std::numeric_limits<double>::max();
+    lowerLimit = -upperLimit;
+  }
+  template <typename S> typename std::enable_if<!std::is_arithmetic<S>::value && std::is_integral<typename S::value_type>::value>::type _clear() {
+    upperLimit.fill(std::numeric_limits<int32_t>::max());
+    lowerLimit = -upperLimit;
+  }
+  template <typename S> typename std::enable_if<   !std::is_arithmetic<S>::value && std::is_floating_point<typename S::value_type>::value>::type _clear() {
+    upperLimit.fill(std::numeric_limits<double>::max());
+    lowerLimit = -upperLimit;
+  }
+
+};
+
+/**
+ * Operator overload (<<) to enable an easy way to print the state of a
+ * Integrator instance to an output stream.\n
+ * Does not print a newline control character.
+ */
+template <typename T>
+std::ostream& operator<<(std::ostream& os, I<T>& i) {
+  os << "Block integrator: '" << i.getName() << "' is enabled=" << i.enabled;
+  os << ", upperLimit=" << i.upperLimit << ", lowerLimit=" << i.lowerLimit; 
+  return os;
+}
+
+};
 };
 #endif /* ORG_EEROS_CONTROL_I_HPP_ */
--- includes/eeros/control/Sum.hpp
+++ includes/eeros/control/Sum.hpp
@@ -24,10 +24,11 @@ class Sum : public Block1o<T> {
   /**
    * Constructs a sum instance with all inputs to be added.\n
    */
-  Sum() {
+  Sum() : first(true) {
     for(uint8_t i = 0; i < N; i++) {
       negated[i] = false;
       in[i].setOwner(this);
+      init[i] = false;
     }
   }
 
@@ -41,9 +42,20 @@ class Sum : public Block1o<T> {
   */
   virtual void run() {
     T sum; sum = 0; // TODO works only with primitive types or eeros::math::Matrix -> make specialization and use fill() for compatibility with std::array;
-    for(uint8_t i = 0; i < N; i++) {
-      if(negated[i]) sum -= in[i].getSignal().getValue();
-      else sum += in[i].getSignal().getValue();
+    if (first) {
+      for (uint8_t i = 0; i < N; i++) {
+        T val;
+        if (init[i]) val = initVal[i];
+        else val = in[i].getSignal().getValue();
+        if (negated[i]) sum -= val;
+        else sum += val;
+      }
+      first = false;
+    } else {
+      for (uint8_t i = 0; i < N; i++) {
+        if (negated[i]) sum -= in[i].getSignal().getValue();
+        else sum += in[i].getSignal().getValue();
+      }
     }
     this->out.getSignal().setValue(sum);
     this->out.getSignal().setTimestamp(in[0].getSignal().getTimestamp());
@@ -69,13 +81,36 @@ class Sum : public Block1o<T> {
     if (index >= N) throw eeros::Fault("index too big in sum block '" + this->getName() + "'");
     negated[index] = true;
   }
+  
+  /**
+   * Set the initial state of a given input to a sum block. This allows to determine an initial state 
+   * where this initial state would be a nan value in case of a feedback path. This can be helpful when 
+   * the input signal comes from a block which is later in the chain and has not run yet 
+   * therefore delivering a nan signal.
+   *
+   * @see enable()
+   * @param index - index of input
+   * @param val - initial state
+   */
+  virtual void setInitCondition(uint8_t index, T val) {
+    init[index] = true;
+    initVal[index] = val;
+  }
 
- protected:
+
+ private:
   Input<T> in[N];
   bool negated[N];
+  bool first;
+  bool init[N];
+  T initVal[N];
 };
 
-/********** Print functions **********/
+/**
+ * Operator overload (<<) to enable an easy way to print the state of a
+ * Sum instance to an output stream.\n
+ * Does not print a newline control character.
+ */
 template <uint8_t N, typename T>
 std::ostream& operator<<(std::ostream& os, Sum<N,T>& sum) {
   os << "Block sum: '" << sum.getName() << "'"; 
