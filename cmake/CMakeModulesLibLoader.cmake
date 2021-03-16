c684de54e104261fc41964f9c1dcd42b0c7e594c
Andreas Kunz <andreas.kunz@ost.ch>
2021-03-16 17:04:06 +0100

    Improve cmake-modules library usage

--- cmake/CMakeModulesLibLoader.cmake
+++ cmake/CMakeModulesLibLoader.cmake
@@ -13,19 +13,20 @@ function(load_cmake_modules_lib)
   endif()
 
 
-  if(NOT CMAKE_MODULES_LIB_EXISTS)
-    set(CMAKE_MODULES_LIB_URL https://github.com/andreaskunz/cmake-modules.git CACHE STRING "The path to the origin of the cmake-modules library")
+  if(NOT LIB_CMAKE_MODULES_EXISTS)
+    set(LIB_CMAKE_MODULES_URL https://github.com/andreaskunz/cmake-modules.git CACHE STRING "The path to the origin of the cmake-modules library")
 
-    execute_process(COMMAND ${GIT_EXECUTABLE} clone ${CMAKE_MODULES_LIB_URL}
+    execute_process(COMMAND ${GIT_EXECUTABLE} clone ${LIB_CMAKE_MODULES_URL}
                     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-                    RESULT_VARIABLE CMAKE_MODULES_LIB_CLONE_RESULT
-                    ERROR_VARIABLE CMAKE_MODULES_LIB_CLONE_OUTPUT)
-    if(NOT CMAKE_MODULES_LIB_CLONE_RESULT EQUAL "0")
-      message(FATAL_ERROR "failed command: git clone ${CMAKE_MODULES_LIB_URL}\n ${CMAKE_MODULES_LIB_CLONE_OUTPUT}")
+                    RESULT_VARIABLE LIB_CMAKE_MODULES_CLONE_RESULT
+                    ERROR_VARIABLE LIB_CMAKE_MODULES_CLONE_OUTPUT)
+    if(NOT LIB_CMAKE_MODULES_CLONE_RESULT EQUAL "0")
+      message(FATAL_ERROR "failed command: git clone ${LIB_CMAKE_MODULES_URL}\n ${LIB_CMAKE_MODULES_CLONE_OUTPUT}")
     endif()
 
-    set(CMAKE_MODULES_LIB_PATH "${CMAKE_BINARY_DIR}/cmake-modules" CACHE PATH "The path to the cmake-modules library")
-    set(CMAKE_MODULES_LIB_EXISTS TRUE CACHE BOOL "TRUE if the cmake-modules library was cloned successfully")
+    list(APPEND CMAKE_MODULE_PATH "${CMAKE_BINARY_DIR}/cmake-modules")
+    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} CACHE PATH "List of directories specifying a search path for CMake modules to be loaded" FORCE)
+    set(LIB_CMAKE_MODULES_EXISTS TRUE CACHE BOOL "TRUE if the cmake-modules library was cloned successfully")
   endif()
 endfunction()
 
