ac925075c6b66356aedd656b36237a0429479c66
Moritz Lammerich <moritz.lammerich@ost.ch>
2022-03-02 10:19:02 +0100

    Fix CMake configuration errors
    - The install include path was set to `include/eeros` instead of `include`
      causing headers to not be found
    - EEROSConfig.cmake always searched for "ecmasterlib" even when building
      without it causing dependency errors when building EEROS applications and
      ecmasterlib not being available because it's not actually needed
    
    The include paths are a simple fix and EEROSConfig.cmake now checks eeros's
    `TARGET_COMPILE_DEFINITIONS` for `USE_ETHERCAT` to determine if
    `find_dependency(ecmasterlib)` is needed.

--- EEROSConfig.cmake
+++ EEROSConfig.cmake
@@ -1,3 +1,8 @@
 include(CMakeFindDependencyMacro)
-find_dependency(ecmasterlib)
 include(${CMAKE_CURRENT_LIST_DIR}/EEROS.cmake)
+
+get_target_property(eeros_interface_comp_defs eeros "INTERFACE_COMPILE_DEFINITIONS")
+
+if("USE_ETHERCAT" IN_LIST eeros_interface_comp_defs)
+  find_dependency(ecmasterlib)
+endif()
