761c5eb9afea5bd41e7289b1985c84cb3feb6278
ursgraf <urs.graf@ost.ch>
2023-03-01 09:36:16 +0100

    Add test for non default template parameter

--- test/control/Sum.cpp
+++ test/control/Sum.cpp
@@ -3,6 +3,7 @@
 #include <eeros/math/Matrix.hpp>
 #include <eeros/core/Fault.hpp>
 #include <gtest/gtest.h>
+#include <Utils.hpp>
 
 using namespace eeros;
 using namespace eeros::control;
@@ -10,81 +11,100 @@ using namespace eeros::math;
 
 // Test initial values for NaN
 TEST(controlSumTest, initialValue) {
-	Sum<> s1;
-	Sum<2, int> s2;
-	Sum<2, Matrix<2,1,double>> s3;
-	Sum<2, Matrix<2,1,int>> s4;
-	s1.setName("s1");
-	s2.setName("s2");
-	s3.setName("s3");
-	s4.setName("s4");
-		
-	EXPECT_TRUE(std::isnan(s1.getOut().getSignal().getValue()));
-	EXPECT_EQ(s2.getOut().getSignal().getValue(), std::numeric_limits<int32_t>::min());
-	EXPECT_TRUE(std::isnan(s3.getOut().getSignal().getValue()[0]));
-	EXPECT_TRUE(std::isnan(s3.getOut().getSignal().getValue()[1]));
-	EXPECT_EQ(s4.getOut().getSignal().getValue()[0], std::numeric_limits<int32_t>::min());
-	EXPECT_EQ(s4.getOut().getSignal().getValue()[1], std::numeric_limits<int32_t>::min());
-	try {
-		s1.run();
-		FAIL();
-	} catch(eeros::Fault const & err) {
-		EXPECT_EQ(err.what(), std::string("Read from an unconnected input in block 's1'"));
-	}
-	try {
-		s2.run();
-		FAIL();
-	} catch(eeros::Fault const & err) {
-		EXPECT_EQ(err.what(), std::string("Read from an unconnected input in block 's2'"));
-	}
-	try {
-		s3.run();
-		FAIL();
-	} catch(eeros::Fault const & err) {
-		EXPECT_EQ(err.what(), std::string("Read from an unconnected input in block 's3'"));
-	}
-	try {
-		s4.run();
-		FAIL();
-	} catch(eeros::Fault const & err) {
-		EXPECT_EQ(err.what(), std::string("Read from an unconnected input in block 's4'"));
-	}
+  Sum<> s1;
+  Sum<2, int> s2;
+  Sum<2, Matrix<2,1,double>> s3;
+  Sum<2, Matrix<2,1,int>> s4;
+  s1.setName("s1");
+  s2.setName("s2");
+  s3.setName("s3");
+  s4.setName("s4");
+      
+  EXPECT_TRUE(std::isnan(s1.getOut().getSignal().getValue()));
+  EXPECT_EQ(s2.getOut().getSignal().getValue(), std::numeric_limits<int32_t>::min());
+  EXPECT_TRUE(std::isnan(s3.getOut().getSignal().getValue()[0]));
+  EXPECT_TRUE(std::isnan(s3.getOut().getSignal().getValue()[1]));
+  EXPECT_EQ(s4.getOut().getSignal().getValue()[0], std::numeric_limits<int32_t>::min());
+  EXPECT_EQ(s4.getOut().getSignal().getValue()[1], std::numeric_limits<int32_t>::min());
+  try {
+    s1.run();
+    FAIL();
+  } catch(eeros::Fault const & err) {
+    EXPECT_EQ(err.what(), std::string("Read from an unconnected input in block 's1'"));
+  }
+  try {
+    s2.run();
+    FAIL();
+  } catch(eeros::Fault const & err) {
+    EXPECT_EQ(err.what(), std::string("Read from an unconnected input in block 's2'"));
+  }
+  try {
+    s3.run();
+    FAIL();
+  } catch(eeros::Fault const & err) {
+    EXPECT_EQ(err.what(), std::string("Read from an unconnected input in block 's3'"));
+  }
+  try {
+    s4.run();
+    FAIL();
+  } catch(eeros::Fault const & err) {
+    EXPECT_EQ(err.what(), std::string("Read from an unconnected input in block 's4'"));
+  }
 }
 
 TEST(controlSumTest, index) {
-	Sum<2, int> s1;
-	s1.setName("s1");
-	Constant<int> c1(10);
-	s1.getIn(0).connect(c1.getOut());
-	s1.getIn(1).connect(c1.getOut());
-	try {
-		s1.getIn(2).connect(c1.getOut());
-		FAIL();
-	} catch(eeros::Fault const & err) {
-		EXPECT_EQ(err.what(), std::string("Trying to get inexistent element of input vector in block 's1'"));
-	}
-	
-	s1.negateInput(0);
-	s1.negateInput(1);
-	try {
-		s1.negateInput(2);
-		FAIL();
-	} catch(eeros::Fault const & err) {
-		EXPECT_EQ(err.what(), std::string("Trying to get inexistent element of input vector in block 's1'"));
-	}
+  Sum<2, int> s1;
+  s1.setName("s1");
+  Constant<int> c1(10);
+  s1.getIn(0).connect(c1.getOut());
+  s1.getIn(1).connect(c1.getOut());
+  try {
+    s1.getIn(2).connect(c1.getOut());
+    FAIL();
+  } catch(eeros::Fault const & err) {
+    EXPECT_EQ(err.what(), std::string("Trying to get inexistent element of input vector in block 's1'"));
+  }
+  
+  s1.negateInput(0);
+  s1.negateInput(1);
+  try {
+    s1.negateInput(2);
+    FAIL();
+  } catch(eeros::Fault const & err) {
+    EXPECT_EQ(err.what(), std::string("Trying to get inexistent element of input vector in block 's1'"));
+  }
 }
 
 TEST(controlSumTest, int) {
-	Sum<2, int> s1;
-	Constant<int> c1(10), c2(-325);
-	s1.getIn(0).connect(c1.getOut());
-	s1.getIn(1).connect(c2.getOut());
-	c1.run(); c2.run(); s1.run();	
-	EXPECT_EQ(s1.getOut().getSignal().getValue(), -315);
-	s1.negateInput(0);
-	s1.run();
-	EXPECT_EQ(s1.getOut().getSignal().getValue(), -335);
-	s1.negateInput(1);
-	s1.run();
-	EXPECT_EQ(s1.getOut().getSignal().getValue(), 315);
+  Sum<2, int> s1;
+  Constant<int> c1(10), c2(-325);
+  s1.getIn(0).connect(c1.getOut());
+  s1.getIn(1).connect(c2.getOut());
+  c1.run(); c2.run(); s1.run();	
+  EXPECT_EQ(s1.getOut().getSignal().getValue(), -315);
+  s1.negateInput(0);
+  s1.run();
+  EXPECT_EQ(s1.getOut().getSignal().getValue(), -335);
+  s1.negateInput(1);
+  s1.run();
+  EXPECT_EQ(s1.getOut().getSignal().getValue(), 315);
+}
+
+TEST(controlSumTest, double) {
+  Sum<3, double> s1;
+  Constant<> c1(2.5), c2(-3.5), c3(10.1);
+  s1.getIn(0).connect(c1.getOut());
+  s1.getIn(1).connect(c2.getOut());
+  s1.getIn(2).connect(c3.getOut());
+  c1.run(); c2.run(); c3.run(); s1.run();	
+  EXPECT_TRUE(Utils::compareApprox(s1.getOut().getSignal().getValue(), 9.1, 0.001));  
+  s1.negateInput(0);
+  s1.run();
+  EXPECT_TRUE(Utils::compareApprox(s1.getOut().getSignal().getValue(), 4.1, 0.001));  
+  s1.negateInput(1);
+  s1.run();
+  EXPECT_TRUE(Utils::compareApprox(s1.getOut().getSignal().getValue(), 11.1, 0.001));  
+  s1.negateInput(2);
+  s1.run();
+  EXPECT_TRUE(Utils::compareApprox(s1.getOut().getSignal().getValue(), -9.1, 0.001));  
 }
