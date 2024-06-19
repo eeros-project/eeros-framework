3d05916b617516b018ebe6693be817a29ad0e870
Urs <urs.graf@ost.ch>
2024-06-19 08:57:24 +0200

    Add log output when private safety event is called from public context

--- src/safety/SafetyLevel.cpp
+++ src/safety/SafetyLevel.cpp
@@ -1,117 +1,110 @@
 #include <eeros/safety/SafetyLevel.hpp>
 
 namespace eeros {
-	namespace safety {
+namespace safety {
 
-		SafetyEvent::SafetyEvent(std::string description) : description(description) {
-			static int count = 0;
-			id = count++;
-		}
+SafetyEvent::SafetyEvent(std::string description) : description(description) {
+  static int count = 0;
+  id = count++;
+}
 
-		SafetyEvent::~SafetyEvent() {
-		}
+std::string SafetyEvent::getDescription() {
+  return description;
+}
 
-		std::string SafetyEvent::getDescription() {
-			return description;
-		}
+SafetyLevel::SafetyLevel(std::string description) : description(description), log(logger::Logger::getLogger('S')) {
+  // number the levels when adding them to the safety system
+}
 
-		SafetyLevel::SafetyLevel(std::string description) : description(description) {
-			// number the levels when adding them to the safety system
-		}
+bool SafetyLevel::operator<(const SafetyLevel& level) {
+  return this->id < level.id;
+}
 
-		SafetyLevel::~SafetyLevel() {
-			// nothing to do...
-		}
+bool SafetyLevel::operator<=(const SafetyLevel& level) {
+  return this->id <= level.id;
+}
 
-		bool SafetyLevel::operator<(const SafetyLevel& level) {
-			return this->id < level.id;
-		}
+bool SafetyLevel::operator>(const SafetyLevel& level) {
+  return this->id > level.id;
+}
 
-		bool SafetyLevel::operator<=(const SafetyLevel& level) {
-			return this->id <= level.id;
-		}
+bool SafetyLevel::operator>=(const SafetyLevel& level) {
+  return this->id >= level.id;
+}
 
-		bool SafetyLevel::operator>(const SafetyLevel& level) {
-			return this->id > level.id;
-		}
+bool SafetyLevel::operator==(const SafetyLevel& level) {
+  return this->id == level.id;
+}
 
-		bool SafetyLevel::operator>=(const SafetyLevel& level) {
-			return this->id >= level.id;
-		}
+bool SafetyLevel::operator!=(const SafetyLevel& level) {
+  return this->id != level.id;
+}
 
-		bool SafetyLevel::operator==(const SafetyLevel& level) {
-			return this->id == level.id;
-		}
+std::string SafetyLevel::getDescription() {
+  return description;
+}
 
-		bool SafetyLevel::operator!=(const SafetyLevel& level) {
-			return this->id != level.id;
-		}
+uint32_t SafetyLevel::getLevelId() {
+  return id;
+}
 
-		std::string SafetyLevel::getDescription() {
-			return description;
-		}
+uint32_t SafetyLevel::getNofActivations() {
+  return nofActivations;
+}
 
-		uint32_t SafetyLevel::getLevelId() {
-			return id;
-		}
+SafetyLevel* SafetyLevel::getDestLevelForEvent(SafetyEvent event, bool privateEventOk) {
+  auto it = transitions.find(event.id);
+  if(it != transitions.end()) {
+    if((it->second.second != kPrivateEvent) || privateEventOk) return it->second.first;
+    else log.error() << "triggering private event \'" << event << "\' from nonprivate context";
+  }
+  return nullptr;
+}
 
-		uint32_t SafetyLevel::getNofActivations() {
-			return nofActivations;
-		}
+void SafetyLevel::addEvent(SafetyEvent event, SafetyLevel& nextLevel, EventType type) {
+  transitions.insert(std::make_pair(event.id, std::make_pair(&nextLevel, type)));
+}
 
-		SafetyLevel* SafetyLevel::getDestLevelForEvent(SafetyEvent event, bool privateEventOk) {
-			auto it = transitions.find(event.id);
-			if(it != transitions.end()) {
-				if((it->second.second != kPrivateEvent) || privateEventOk) return it->second.first;
-			}
-			return nullptr;
-		}
+void SafetyLevel::setLevelAction(std::function<void (SafetyContext*)> action) {
+  this->action = action;
+}
 
-		void SafetyLevel::addEvent(SafetyEvent event, SafetyLevel& nextLevel, EventType type) {
-			transitions.insert(std::make_pair(event.id, std::make_pair(&nextLevel, type)));
-		}
+void SafetyLevel::setInputAction(InputAction* action) {
+  inputAction.push_back(action);
+}
 
-		void SafetyLevel::setLevelAction(std::function<void (SafetyContext*)> action) {
-			this->action = action;
-		}
+void SafetyLevel::setInputActions(std::vector<InputAction*> actionList) {
+  inputAction = actionList;
+}
 
+void SafetyLevel::setOutputAction(OutputAction* action) {
+  outputAction.push_back(action);
+}
 
-		void SafetyLevel::setInputAction(InputAction* action) {
-			inputAction.push_back(action);
-		}
+void SafetyLevel::setOutputActions(std::vector<OutputAction*> actionList) {
+  outputAction = actionList;
+}
 
-		void SafetyLevel::setInputActions(std::vector<InputAction*> actionList) {
-			inputAction = actionList;
-		}
+/********** Print functions **********/
+std::ostream& operator<<(std::ostream& os, eeros::safety::SafetyEvent& event) {
+  os << event.getDescription();
+  return os;
+}
 
-		void SafetyLevel::setOutputAction(OutputAction* action) {
-			outputAction.push_back(action);
-		}
+std::ostream& operator<<(std::ostream& os, eeros::safety::SafetyEvent* event) {
+  os << event->getDescription();
+  return os;
+}
 
-		void SafetyLevel::setOutputActions(std::vector<OutputAction*> actionList) {
-			outputAction = actionList;
-		}
+std::ostream& operator<<(std::ostream& os, eeros::safety::SafetyLevel& level) {
+  os << level.getDescription();
+  return os;
+}
 
-		/********** Print functions **********/
-		std::ostream& operator<<(std::ostream& os, eeros::safety::SafetyEvent& event) {
-			os << event.getDescription();
-			return os;
-		}
-
-		std::ostream& operator<<(std::ostream& os, eeros::safety::SafetyEvent* event) {
-			os << event->getDescription();
-			return os;
-		}
-
-		std::ostream& operator<<(std::ostream& os, eeros::safety::SafetyLevel& level) {
-			os << level.getDescription();
-			return os;
-		}
-
-		std::ostream& operator<<(std::ostream& os, eeros::safety::SafetyLevel* level) {
-			os << level->getDescription();
-			return os;
-		}
-
-	};
+std::ostream& operator<<(std::ostream& os, eeros::safety::SafetyLevel* level) {
+  os << level->getDescription();
+  return os;
 };
+
+}
+}
