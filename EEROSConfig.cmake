1b538c87b2aaa4298c2aaab34b646f92dbc19542
Moritz Lammerich <moritz.lammerich@ost.ch>
2024-09-19 15:50:27 +0200

    put eeros cmake targets into EEROS namespace

--- EEROSConfig.cmake
+++ EEROSConfig.cmake
@@ -1,13 +1,13 @@
 include(CMakeFindDependencyMacro)
 include(${CMAKE_CURRENT_LIST_DIR}/EEROS.cmake)
 
-get_target_property(eeros_interface_comp_defs eeros "INTERFACE_COMPILE_DEFINITIONS")
+get_target_property(eeros_interface_comp_defs EEROS::eeros "INTERFACE_COMPILE_DEFINITIONS")
 
 if("USE_ETHERCAT" IN_LIST eeros_interface_comp_defs)
   find_dependency(ecmasterlib)
 endif()
 
-get_target_property(eeros_interface_libs eeros "INTERFACE_LINK_LIBRARIES")
+get_target_property(eeros_interface_libs EEROS::eeros "INTERFACE_LINK_LIBRARIES")
 
 foreach(dependency IN LISTS eeros_interface_libs)
   message("found EEROS dep: ${dependency}")
