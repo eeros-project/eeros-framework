c5a7cde6bcfa8ea7b0f48a8c2fc6f168e69e54da
Moritz Lammerich <moritz.lammerich@ost.ch>
2024-09-19 12:46:53 +0200

    use FetchContent for libucl

--- EEROSConfig.cmake
+++ EEROSConfig.cmake
@@ -6,3 +6,13 @@ get_target_property(eeros_interface_comp_defs eeros "INTERFACE_COMPILE_DEFINITIO
 if("USE_ETHERCAT" IN_LIST eeros_interface_comp_defs)
   find_dependency(ecmasterlib)
 endif()
+
+get_target_property(eeros_interface_libs eeros "INTERFACE_LINK_LIBRARIES")
+
+foreach(dependency IN LISTS eeros_interface_libs)
+  message("found EEROS dep: ${dependency}")
+  string(FIND ${dependency} :: package_name_end)
+  string(SUBSTRING ${dependency} 0 ${package_name_end} dep_package)
+  message("package name: ${dep_package}")
+  find_dependency(${dep_package})
+endforeach()
