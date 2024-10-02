ad16d513eeccdfb51f00d26c2220a0bb31fe62ba
Moritz Lammerich <moritz.lammerich@ost.ch>
2024-10-02 12:58:46 +0200

    fix libucl not properly propagating to depenents
    The core issue was `ucl` being linked privately, even though its headers
    are needed by parts of the EEROS API. However, publicly linking ucl
    unearthed some limitations in our dependency handling.
    
    First, our libucl fork did not install any CMake targets. Seeing that the
    current master of libucl does, I updated our fork to be in sync with
    upstream. However, CMake was still complaing about no finding `ucl::ucl`,
    even though `find_dependency` (correctly) claimed to find it.
    
    The issue turned out  to be in the order of operations: Since `ucl::ucl`
    is not part of the same export set, EEROS.cmake checks its presence and
    errors out if it's not found. However, EEROSCOnfig.cmake included
    EEROS.cmake *berfore* finding any dependencies, since the dependencies
    are listed in a custom target property that is only availabe *after*
    including EEROS.cmake.
    
    To mitigate this issue, EEROSConfig.cmake is now generated passing in the
    CMake package dependencies to be hard coded into the file (in addition to
    the custom target property) and all `find_depenency` calls happen before
    EEROS.cmake is included. Doing this ensures `ucl::ucl` is available when
    EEROS.cmake runs its checks, thus resolving the rather confusing error.
    
    Note that the EEROS_CMAKE_PACKAGE_DEPENDS is still available and
    populated but does not currently serve any function for consumers of
    EEROSConfig.cmake. It is, however still used for tracking dependencies
    within EEROS itself (i.e. to populate the depenency list in
    EEROSConfig.cmake).

--- cmake/libucl.cmake
+++ cmake/libucl.cmake
@@ -2,8 +2,9 @@ include(FetchContent)
 
 FetchContent_Declare(libucl
     GIT_REPOSITORY https://github.com/eeros-project/libucl.git
-    GIT_TAG 932ff8147b6c809770e01127e4b2edbebcdf1695
+    GIT_TAG master
     SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/libucl-src"
     BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/libucl-build")
 
 FetchContent_MakeAvailable(libucl)
+eeros_add_package(eeros ucl)
--- cmake/package_management.cmake
+++ cmake/package_management.cmake
@@ -1,8 +1,12 @@
 include(FindPkgConfig)
 
 function(eeros_find_package target package)
-    set_property(TARGET "${target}" APPEND PROPERTY EXPORT_PROPERTIES EEROS_CMAKE_PACKAGE_DEPENDS)
     find_package("${package}" REQUIRED)
+    eeros_add_package(${target} ${package})
+endfunction()
+
+function(eeros_add_package target package)
+    set_property(TARGET "${target}" APPEND PROPERTY EXPORT_PROPERTIES EEROS_CMAKE_PACKAGE_DEPENDS)
     set_property(TARGET "${target}" APPEND PROPERTY EEROS_CMAKE_PACKAGE_DEPENDS "${package}")
 endfunction()
 
