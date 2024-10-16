19bcb5a949bbaca33cb28c9e6feeb1c1e0805549
Moritz Lammerich <moritz.lammerich@ost.ch>
2024-10-16 15:55:13 +0200

    fix namespacing issues

--- cmake/driver_config.cmake
+++ cmake/driver_config.cmake
@@ -9,39 +9,39 @@ function(add_driver_option)
     option(${ARG_FLAG_NAME} ${ARG_HELP_TEXT} OFF)
 
     if(${ARG_FLAG_NAME})
-        target_compile_definitions(eeros PUBLIC EEROS_${ARG_FLAG_NAME})
+        target_compile_definitions(${PROJECT_NAME}_eeros PUBLIC EEROS_${ARG_FLAG_NAME})
 
         foreach(package IN LISTS ARG_PACKAGE_DEPENDS)
             message("looking for ${package}")
-            eeros_find_package(eeros ${package})
+            eeros_find_package(${PROJECT_NAME}_eeros ${package})
         endforeach()
 
         if(DEFINED ARG_LINK_LIBRARIES)
             message("linking ${ARG_LINK_LIBRARIES}")
-            target_link_libraries(eeros PRIVATE ${ARG_LINK_LIBRARIES})
+            target_link_libraries(${PROJECT_NAME}_eeros PRIVATE ${ARG_LINK_LIBRARIES})
         endif()
 
         if(DEFINED ARG_LINK_LIBRARIES_PUBLIC)
             message("linking public ${ARG_LINK_LIBRARIES_PUBLIC}")
-            target_link_libraries(eeros PUBLIC "${ARG_LINK_LIBRARIES_PUBLIC}")
+            target_link_libraries(${PROJECT_NAME}_eeros PUBLIC "${ARG_LINK_LIBRARIES_PUBLIC}")
         endif()
 
         if(DEFINED ARG_PKG_CONFIG)
             include(FindPkgConfig)
 
             foreach(pkg IN LISTS ARG_PKG_CONFIG)
-                eeros_find_pkgconfig(eeros "${pkg}")
-                target_link_libraries(eeros PUBLIC "PkgConfig::${pkg}")
+                eeros_find_pkgconfig(${PROJECT_NAME}_eeros "${pkg}")
+                target_link_libraries(${PROJECT_NAME}_eeros PUBLIC "PkgConfig::${pkg}")
             endforeach()
         endif()
 
         if(DEFINED ARG_COMPILE_DEFINITIONS)
             message("adding compile definitions: ${ARG_COMPILE_DEFINITIONS}")
-            target_compile_definitions(eeros PUBLIC ${ARG_COMPILE_DEFINITIONS})
+            target_compile_definitions(${PROJECT_NAME}_eeros PUBLIC ${ARG_COMPILE_DEFINITIONS})
         endif()
 
         if(DEFINED ARG_SOURCES)
-            target_sources(eeros PRIVATE ${ARG_SOURCES})
+            target_sources(${PROJECT_NAME}_eeros PRIVATE ${ARG_SOURCES})
         endif()
 
         foreach(file IN LISTS ARG_CMAKE_FILES)
