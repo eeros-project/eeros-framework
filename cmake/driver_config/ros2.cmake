8bf4a3e6af25992935f3f9581a9108b3cd45da20
Moritz Lammerich <moritz.lammerich@ost.ch>
2024-11-06 12:27:23 +0100

    fix ROS2 cmake config

--- cmake/driver_config/ros2.cmake
+++ cmake/driver_config/ros2.cmake
@@ -3,7 +3,7 @@ include(cmake/package_management.cmake)
 # use $ROS_DISTRO as a simple way to detect if ROS was sourced
 # makes for a much nicer error message for an easy to make mistake
 if(DEFINED ENV{ROS_DISTRO})
-    target_compile_definitions(eeros PUBLIC USE_ROS2)
+    target_compile_definitions(${PROJECT_NAME}_eeros PUBLIC USE_ROS2)
 
     eeros_find_package(${PROJECT_NAME}_eeros ament_cmake)
     eeros_find_package(${PROJECT_NAME}_eeros rclcpp)
