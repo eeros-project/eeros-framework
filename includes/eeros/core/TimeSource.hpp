934e3e89136358a10f9831414adf0249aa1091ae
Moritz Lammerich <moritz.lammerich@ost.ch>
2024-11-29 14:48:07 +0100

    add TimeSource Doxygen comments

--- includes/eeros/core/TimeSource.hpp
+++ includes/eeros/core/TimeSource.hpp
@@ -9,17 +9,38 @@
 namespace eeros {
 namespace core {
 
+/**
+ * Interface for timing and scheduling information
+ */
 class TimeSource {
 public:
   using Uptime = std::chrono::duration<double, std::ratio_multiply<std::ratio<1000>, std::chrono::milliseconds::period>>;
   using Seconds = std::chrono::duration<double, std::chrono::seconds::period>;
+  /**
+   * start time keeping, this should be t=0 for calculating uptime.
+   * Also starts the first cycle
+   */
   virtual void start() noexcept = 0;
+  /**
+   * return the elapsed time since start() was called
+   */
   virtual Uptime currentUptime() noexcept = 0;
+  /**
+   * return the period used by the executor.
+   * The period should be constant over the lifetime of the executor
+   */
   virtual Seconds getPeriod() noexcept = 0;
+  /**
+   * wait until the next cycle should start
+   */
   virtual void sync() = 0;
   virtual ~TimeSource() {}
 };
 
+
+/**
+ * Default TimeSource implementation based on the system steady/monotonic clock
+ */
 class SystemTime : public TimeSource {
   using Clock = std::chrono::steady_clock;
   using TimePoint =
@@ -31,6 +52,11 @@ class SystemTime : public TimeSource {
   Seconds period;
 
 public:
+  /**
+   * Create a new SystemTime TimeSource
+   *
+   * @param period period in seconds
+   */
   explicit SystemTime(double period) : period(period) {}
 
   virtual void start() noexcept override {
@@ -50,8 +76,20 @@ public:
   }
 };
 
+
+/**
+ * Simulate a periodic clock with no relation to real world time
+ *
+ * This
+ */
 class ManualTime : public TimeSource {
 public:
+  /**
+   * Function to be called on every cycle.
+   * Can be used to check if blocks/signals have the expected value
+   *
+   * @param Uptime the timestamp of the current cycle
+   */
   using Callback = std::function<void(Uptime)>;
 
 private:
@@ -79,3 +117,4 @@ public:
 }; // namespace eeros
 
 #endif
+
