4dfe9af458aa80b3073bc8cfcb429dd05cde51c4
Urs Graf <urs.graf@ost.ch>
2025-05-21 15:20:01 +0200

    Add tf tests

--- test/math/tf/Matrix.cpp
+++ test/math/tf/Matrix.cpp
@@ -1,6 +1,7 @@
-#include <eeros/math/tf/TF_Matrix.hpp>
-#include <eeros/core/Fault.hpp>
 #include <gtest/gtest.h>
+
+#include <eeros/core/Fault.hpp>
+#include <eeros/math/tf/TF_Matrix.hpp>
 
 using namespace eeros;
 using namespace eeros::math;
@@ -8,70 +9,76 @@ using namespace eeros::math::tf;
 
 TEST(TF_MatrixTest, roll) {
   TF_Matrix tf1;
-  Matrix<4,1,double> v1;
-  Matrix<3,1,double> v2;
-  double r,p,y;
-  r = M_PI/3;
+  Matrix<4, 1, double> v1;
+  Matrix<3, 1, double> v2;
+  double r, p, y;
+  r = M_PI / 3;
   p = 0.0;
   y = 0.0;
-  tf1.setRPY(r,p,y);
+  tf1.setRPY(r, p, y);
   std::stringstream ss;
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [1 0 0 0]' [0 0.5 0.866025 0]' [0 -0.866025 0.5 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(ss.str(), std::string("[ [1 0 0 0]' [0 0.5 0.866025 0]' [0 "
+                                  "-0.866025 0.5 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
-  EXPECT_EQ(ss.str(), std::string("[1.0472 -0 0]' "));
-  r = -M_PI/3;
-  tf1.setRPY(r,p,y);
+  EXPECT_EQ(ss.str(), std::string("[1.0472 0 0]' "));
+  r = -M_PI / 3;
+  tf1.setRPY(r, p, y);
   ss.str("");
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [1 0 0 0]' [0 0.5 -0.866025 0]' [0 0.866025 0.5 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(ss.str(), std::string("[ [1 0 0 0]' [0 0.5 -0.866025 0]' [0 "
+                                  "0.866025 0.5 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
-  EXPECT_EQ(ss.str(), std::string("[-1.0472 -0 0]' "));
-  r = 2*M_PI/3;
-  tf1.setRPY(r,p,y);
+  EXPECT_EQ(ss.str(), std::string("[-1.0472 0 0]' "));
+  r = 2 * M_PI / 3;
+  tf1.setRPY(r, p, y);
   ss.str("");
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [1 0 0 0]' [0 -0.5 0.866025 0]' [0 -0.866025 -0.5 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(ss.str(), std::string("[ [1 0 0 0]' [0 -0.5 0.866025 0]' [0 "
+                                  "-0.866025 -0.5 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
-  EXPECT_EQ(ss.str(), std::string("[2.0944 -0 0]' "));
-  r = -2*M_PI/3;
-  tf1.setRPY(r,p,y);
+  EXPECT_EQ(ss.str(), std::string("[2.0944 0 0]' "));
+  r = -2 * M_PI / 3;
+  tf1.setRPY(r, p, y);
   ss.str("");
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [1 0 0 0]' [0 -0.5 -0.866025 0]' [0 0.866025 -0.5 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(ss.str(), std::string("[ [1 0 0 0]' [0 -0.5 -0.866025 0]' [0 "
+                                  "0.866025 -0.5 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
-  EXPECT_EQ(ss.str(), std::string("[-2.0944 -0 0]' "));
+  EXPECT_EQ(ss.str(), std::string("[-2.0944 0 0]' "));
 }
 
 TEST(TF_MatrixTest, pitch) {
   TF_Matrix tf1;
-  Matrix<4,1,double> v1;
-  Matrix<3,1,double> v2;
-  double r,p,y;
+  Matrix<4, 1, double> v1;
+  Matrix<3, 1, double> v2;
+  double r, p, y;
   r = 0;
-  p = M_PI/3;
+  p = M_PI / 3;
   y = 0;
-  tf1.setRPY(r,p,y);
+  tf1.setRPY(r, p, y);
   std::stringstream ss;
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [0.5 0 -0.866025 0]' [0 1 0 0]' [0.866025 0 0.5 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(ss.str(), std::string("[ [0.5 0 -0.866025 0]' [0 1 0 0]' [0.866025 "
+                                  "0 0.5 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
   EXPECT_EQ(ss.str(), std::string("[0 1.0472 0]' "));
-  p = -M_PI/3;
-  tf1.setRPY(r,p,y);
+  p = -M_PI / 3;
+  tf1.setRPY(r, p, y);
   ss.str("");
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [0.5 0 0.866025 0]' [0 1 0 0]' [-0.866025 0 0.5 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(ss.str(), std::string("[ [0.5 0 0.866025 0]' [0 1 0 0]' [-0.866025 "
+                                  "0 0.5 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
@@ -80,96 +87,138 @@ TEST(TF_MatrixTest, pitch) {
 
 TEST(TF_MatrixTest, yaw) {
   TF_Matrix tf1;
-  Matrix<4,1,double> v1;
-  Matrix<3,1,double> v2;
-  double r,p,y;
+  Matrix<4, 1, double> v1;
+  Matrix<3, 1, double> v2;
+  double r, p, y;
   r = 0;
   p = 0.0;
-  y = M_PI/3;
-  tf1.setRPY(r,p,y);
+  y = M_PI / 3;
+  tf1.setRPY(r, p, y);
   std::stringstream ss;
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [0.5 0.866025 0 0]' [-0.866025 0.5 0 0]' [0 0 1 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(ss.str(), std::string("[ [0.5 0.866025 0 0]' [-0.866025 0.5 0 0]' "
+                                  "[0 0 1 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
-  EXPECT_EQ(ss.str(), std::string("[0 -0 1.0472]' "));
-  y = -M_PI/3;
-  tf1.setRPY(r,p,y);
+  EXPECT_EQ(ss.str(), std::string("[0 0 1.0472]' "));
+  y = -M_PI / 3;
+  tf1.setRPY(r, p, y);
   ss.str("");
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [0.5 -0.866025 0 0]' [0.866025 0.5 0 0]' [0 0 1 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(ss.str(), std::string("[ [0.5 -0.866025 0 0]' [0.866025 0.5 0 0]' "
+                                  "[0 0 1 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
-  EXPECT_EQ(ss.str(), std::string("[0 -0 -1.0472]' "));
-  y = 2*M_PI/3;
-  tf1.setRPY(r,p,y);
+  EXPECT_EQ(ss.str(), std::string("[0 0 -1.0472]' "));
+  y = 2 * M_PI / 3;
+  tf1.setRPY(r, p, y);
   ss.str("");
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [-0.5 0.866025 0 0]' [-0.866025 -0.5 0 0]' [0 0 1 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(ss.str(), std::string("[ [-0.5 0.866025 0 0]' [-0.866025 -0.5 0 "
+                                  "0]' [0 0 1 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
-  EXPECT_EQ(ss.str(), std::string("[0 -0 2.0944]' "));
-  y= -2*M_PI/3;
-  tf1.setRPY(r,p,y);
+  EXPECT_EQ(ss.str(), std::string("[0 0 2.0944]' "));
+  y = -2 * M_PI / 3;
+  tf1.setRPY(r, p, y);
   ss.str("");
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [-0.5 -0.866025 0 0]' [0.866025 -0.5 0 0]' [0 0 1 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(ss.str(), std::string("[ [-0.5 -0.866025 0 0]' [0.866025 -0.5 0 "
+                                  "0]' [0 0 1 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
-  EXPECT_EQ(ss.str(), std::string("[0 -0 -2.0944]' "));
+  EXPECT_EQ(ss.str(), std::string("[0 0 -2.0944]' "));
 }
 
 TEST(TF_MatrixTest, mix) {
   TF_Matrix tf1;
-  Matrix<4,1,double> v1;
-  Matrix<3,1,double> v2;
-  double r,p,y;
-  r = M_PI/4;
-  p = M_PI/3;
-  y = M_PI/2;
-  tf1.setRPY(r,p,y);
+  Matrix<4, 1, double> v1;
+  Matrix<3, 1, double> v2;
+  double r, p, y;
+  r = M_PI / 4;
+  p = M_PI / 3;
+  y = M_PI / 2;
+  tf1.setRPY(r, p, y);
   std::stringstream ss;
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [3.06162e-17 0.5 -0.866025 0]' [-0.707107 0.612372 0.353553 0]' [0.707107 0.612372 0.353553 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(
+      ss.str(),
+      std::string("[ [3.06162e-17 0.5 -0.866025 0]' [-0.707107 0.612372 "
+                  "0.353553 0]' [0.707107 0.612372 0.353553 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
   EXPECT_EQ(ss.str(), std::string("[0.785398 1.0472 1.5708]' "));
-  r = -M_PI/4;
-  p = -M_PI/3;
-  y = -M_PI/5;
-  tf1.setRPY(r,p,y);
+  r = -M_PI / 4;
+  p = -M_PI / 3;
+  y = -M_PI / 5;
+  tf1.setRPY(r, p, y);
   ss.str("");
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [0.404508 -0.293893 0.866025 0]' [0.911047 0.212118 -0.353553 0]' [-0.0797928 0.932005 0.353553 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(
+      ss.str(),
+      std::string(
+          "[ [0.404508 -0.293893 0.866025 0]' [0.911047 0.212118 -0.353553 0]' "
+          "[-0.0797928 0.932005 0.353553 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
   EXPECT_EQ(ss.str(), std::string("[-0.785398 -1.0472 -0.628319]' "));
-  r = -M_PI/4;
-  p = -M_PI/5;
-  y = 5*M_PI/6;
-  tf1.setRPY(r,p,y);
+  r = -M_PI / 4;
+  p = -M_PI / 5;
+  y = 5 * M_PI / 6;
+  tf1.setRPY(r, p, y);
   ss.str("");
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [-0.700629 0.404508 0.587785 0]' [-0.713497 -0.404559 -0.572061 0]' [0.0063901 -0.820186 0.572061 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(
+      ss.str(),
+      std::string(
+          "[ [-0.700629 0.404508 0.587785 0]' [-0.713497 -0.404559 -0.572061 "
+          "0]' [0.0063901 -0.820186 0.572061 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
   EXPECT_EQ(ss.str(), std::string("[-0.785398 -0.628319 2.61799]' "));
-  r = 3*M_PI/4;
-  p = M_PI/4;
-  y = -5*M_PI/6;
-  tf1.setRPY(r,p,y);
+  r = 3 * M_PI / 4;
+  p = M_PI / 4;
+  y = -5 * M_PI / 6;
+  tf1.setRPY(r, p, y);
   ss.str("");
-  ss << tf1;
-  EXPECT_EQ(ss.str(), std::string("[ [-0.612372 -0.353553 -0.707107 0]' [-0.786566 0.362372 0.5 0]' [0.0794593 0.862372 -0.5 0]' [0 0 0 1]' ]"));
+  ss << tf1.getMatrix();
+  EXPECT_EQ(
+      ss.str(),
+      std::string("[ [-0.612372 -0.353553 -0.707107 0]' [-0.786566 0.362372 "
+                  "0.5 0]' [0.0794593 0.862372 -0.5 0]' [0 0 0 1]' ]"));
   v2 = tf1.getRPY();
   ss.str("");
   ss << v2;
   EXPECT_EQ(ss.str(), std::string("[2.35619 0.785398 -2.61799]' "));
+}
+
+TEST(TF_MatrixTest, inv) {
+  TF_Matrix tf1;
+  tf1.setMatrix({0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+  Matrix<4, 4, double> m1 = tf1.inverse();
+  Matrix<4, 4, double> m2;
+  m2.eye();
+  EXPECT_EQ(m1, m2);
+  tf1.setMatrix(m2);
+  m1 = tf1.inverse();
+  EXPECT_EQ(m1, m2);
+  tf1.setMatrix({2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1});
+  m1 = tf1.inverse();
+  m2 = {0.5, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
+  EXPECT_EQ(m1, m2);
+  tf1.setMatrix({2, 0, 5, 0.25, 1, 1, 0, 0, 0, 0.5, 1, 0, 1, 0.5, 0, 1});
+  m1 = tf1.inverse();
+  std::stringstream ss;
+  ss << m1;
+  EXPECT_EQ(ss.str(),
+            std::string("[ [0.228571 0.6 -1.14286 -0.0571429]' [-0.228571 0.4 "
+                        "1.14286 0.0571429]' [0.114286 -0.2 0.428571 "
+                        "-0.0285714]' [-0.114286 -0.8 0.571429 1.02857]' ]"));
 }
--- test/math/tf/Tree.cpp
+++ test/math/tf/Tree.cpp
@@ -1,30 +1,32 @@
-#include <eeros/math/tf/TF_Tree.hpp>
-#include <eeros/core/Fault.hpp>
 #include <gtest/gtest.h>
+
+#include <eeros/core/Fault.hpp>
+#include <eeros/math/tf/TF_Tree.hpp>
 
 using namespace eeros;
 using namespace eeros::math;
 using namespace eeros::math::tf;
 
-// Testing constructors with initialization values
 TEST(TF_TreeTest, init1) {
   TF_Tree& tfTree = TF_Tree::instance();
   tfTree.addTF("center", "global");
   tfTree.addTF("head", "center");
   tfTree.addTF("shoulderLeft", "center");
   tfTree.addTF("shoulderRight", "center");
-  tfTree.addTF("eyeLeft","head");
-  tfTree.addTF("eyeRight","head");
+  tfTree.addTF("eyeLeft", "head");
+  tfTree.addTF("eyeRight", "head");
   tfTree.addTF("ellbowLeft", "shoulderLeft");
-  tfTree.addTF("handLeft","ellbowLeft");
+  tfTree.addTF("handLeft", "ellbowLeft");
   tfTree.addTF("ellbowRight", "shoulderRight");
-  tfTree.addTF("handRight","ellbowRight");
+  tfTree.addTF("handRight", "ellbowRight");
 
   try {
     tfTree.addTF("center", "head");
     FAIL();
-  } catch(eeros::Fault const & err) {
-    EXPECT_EQ(err.what(), std::string("TF_Tree: could not create TF because a TF with name \"center\" already exists "));
+  } catch (eeros::Fault const& err) {
+    EXPECT_EQ(err.what(),
+              std::string("TF_Tree: could not create node because a node with "
+                          "name \"center\" already exists "));
   }
 
   tfTree.getTF("head").setTrans(Vector3(0.0, 0.0, 0.7));
@@ -40,9 +42,100 @@ TEST(TF_TreeTest, init1) {
   try {
     tfTree.getTF("shoulder").setTrans(eeros::math::Vector3(-0.2, 0.0, 0.5));
     FAIL();
-  } catch(eeros::Fault const & err) {
-    EXPECT_EQ(err.what(), std::string("TF_Tree: expected TF \"shoulder\"  could not be found"));
+  } catch (eeros::Fault const& err) {
+    EXPECT_EQ(
+        err.what(),
+        std::string("TF_Tree: expected node \"shoulder\"  could not be found"));
   }
 
-//    TrafoMatrix* tf_eyeRight = tfTree.getTF("eyeRight");
+  TF_Matrix& eyeRight = tfTree.getTF("eyeRight");
+  std::stringstream ss;
+  ss << eyeRight.getMatrix();
+  EXPECT_EQ(ss.str(),
+            std::string("[ [0.978031 0.20846 0 0]' [-0.20846 0.978031 0 0]' [0 "
+                        "0 1 0]' [0.06 -0.02 0.1 1]' ]"));
+
+  TF_Matrix tf1 = tfTree.tfFromTo("head", "center");
+  ss.str("");
+  ss << tf1.getMatrix();
+  EXPECT_EQ(ss.str(),
+            std::string("[ [0.987849 -0.129009 -0.086669 0]' [0.119114 "
+                        "0.986656 -0.111007 0]' [0.0998334 0.0993347 0.990033 "
+                        "0]' [-0.0698834 -0.0695343 -0.693023 1]' ]"));
+
+  TF_Matrix tf2 = tfTree.tfFromTo("eyeLeft", "eyeRight");
+  ss.str("");
+  ss << tf2.getMatrix();
+  EXPECT_EQ(ss.str(),
+            std::string("[ [0.99995 0.00999983 0 0]' [-0.00999983 0.99995 0 "
+                        "0]' [0 0 1 0]' [-0.00794677 -0.0392027 0 1]' ]"));
+
+  TF_Matrix tf3 = tfTree.tfFromTo("ellbowRight", "shoulderRight");
+  ss.str("");
+  ss << tf3.getMatrix();
+  EXPECT_EQ(
+      ss.str(),
+      std::string("[ [1 0 0 0]' [0 1 0 0]' [0 0 1 0]' [-0.4 0 -0.01 1]' ]"));
+
+  // adjoint representation from tf
+  Matrix<6, 6, double> ad_ellbowRight_shoulderRight = tf3.getAdjointRep();
+  ss.str("");
+  ss << ad_ellbowRight_shoulderRight;
+  EXPECT_EQ(
+      ss.str(),
+      std::string("[ [1 0 0 0 0 0]' [0 1 0 0 0 0]' [0 0 1 0 0 0]' [0 -0.01 -0 "
+                  "1 0 0]' [0.01 0 -0.4 0 1 0]' [0 0.4 -0 0 0 1]' ]"));
+
+  // adjoint representation from any matrix
+  ad_ellbowRight_shoulderRight = TF_Matrix::getAdjointRep(tf3.getMatrix());
+  ss.str("");
+  ss << ad_ellbowRight_shoulderRight;
+  EXPECT_EQ(
+      ss.str(),
+      std::string("[ [1 0 0 0 0 0]' [0 1 0 0 0 0]' [0 0 1 0 0 0]' [0 -0.01 -0 "
+                  "1 0 0]' [0.01 0 -0.4 0 1 0]' [0 0.4 -0 0 0 1]' ]"));
+
+  // transform velocity parameter, shoulder movements are measured
+  Matrix<6, 1, double> velShoulder = {0.0, 0.0, 0.0, 0.0, 0.1, 0.3};
+  ss.str("");
+  ss << velShoulder;
+  EXPECT_EQ(ss.str(), std::string("[0 0 0 0 0.1 0.3]' "));
+
+  // with ad matrix the velocities for ellbow can be calculated:
+  Matrix<6, 1, double> velEllbow = ad_ellbowRight_shoulderRight * velShoulder;
+  ss.str("");
+  ss << velEllbow;
+  EXPECT_EQ(ss.str(), std::string("[0.001 0.12 -0.04 0 0.1 0.3]' "));
+
+  // global positioning
+  tfTree.getTF("center").setTrans(Vector3(10.0, 5.0, 0.3));
+  tfTree.getTF("center").setRPY(0.0, 0.5, 1.7);
+  // hand shoulder right in global view:
+  TF_Matrix tf_shoulderRight_global =
+      tfTree.tfFromTo("global", "shoulderRight");
+  ss.str("");
+  ss << tf_shoulderRight_global.getMatrix();
+  EXPECT_EQ(
+      ss.str(),
+      std::string(
+          "[ [-0.113072 0.870268 -0.479426 0]' [-0.991665 -0.128844 0 0]' "
+          "[-0.0617713 0.475429 0.877583 0]' [9.99173 5.06366 0.834676 1]' ]"));
+}
+
+TEST(TF_TreeTest, init2) {
+  TF_Tree& tfTree = TF_Tree::instance();
+  try {
+    tfTree.addTF("center", "head");
+    FAIL();
+  } catch (eeros::Fault const& err) {
+    EXPECT_EQ(err.what(),
+              std::string("TF_Tree: could not create node because a node with "
+              "name \"center\" already exists "));
+  }
+  TF_Matrix& eyeRight = tfTree.getTF("eyeRight");
+  std::stringstream ss;
+  ss << eyeRight.getMatrix();
+  EXPECT_EQ(ss.str(),
+            std::string("[ [0.978031 0.20846 0 0]' [-0.20846 0.978031 0 0]' [0 "
+            "0 1 0]' [0.06 -0.02 0.1 1]' ]"));
 }
