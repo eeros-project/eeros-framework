af8a3f654089d73707710d69986c8a46295263e6
Urs Graf <urs.graf@ost.ch>
2026-03-26 14:34:55 +0100

    Remove unnecessary template parameter

--- includes/eeros/control/DeMux.hpp
+++ includes/eeros/control/DeMux.hpp
@@ -4,8 +4,7 @@
 #include <eeros/control/Blockio.hpp>
 #include <eeros/math/Matrix.hpp>
 
-namespace eeros {
-namespace control {
+namespace eeros::control {
  
 /**
  * A demultiplexer block is used to split an input vector 
@@ -13,14 +12,13 @@ namespace control {
  *
  * @tparam N - number of outputs
  * @tparam T - output signal data type (double - default type)
- * @tparam C - input signal data type (Matrix<N,1,T> - default type)
  * @tparam U - signal unit type (dimensionless - default type)
  *
  * @since v0.6
  */
 
-template < uint32_t N, typename T = double, typename C = eeros::math::Matrix<N,1,T>, SIUnit U = SIUnit::create() >
-class DeMux: public Blockio<1,N,C,T,MakeUnitArray<U>::value,MakeUnitArray<U,N>::value> {
+template < uint32_t N, typename T = double, SIUnit U = SIUnit::create() >
+class DeMux: public Blockio<1,N,eeros::math::Matrix<N,1,T>,T,MakeUnitArray<U>::value,MakeUnitArray<U,N>::value> {
  public:
   /**
    * Constructs a demultiplexer instance.
@@ -50,13 +48,12 @@ class DeMux: public Blockio<1,N,C,T,MakeUnitArray<U>::value,MakeUnitArray<U,N>::
  * Demultiplexer instance to an output stream.\n
  * Does not print a newline control character.
  */
-template < uint32_t N, typename T, typename C, SIUnit U >
-std::ostream& operator<<(std::ostream& os, const DeMux<N, T, C, U>& d) {
+template < uint32_t N, typename T, SIUnit U >
+std::ostream& operator<<(std::ostream& os, const DeMux<N,T,U>& d) {
   os << "Block demultiplexer: '" << d.getName() << "'"; 
   return os;
 }
 
-}
 }
 
 #endif /* ORG_EEROS_CONTROL_DEMUX_HPP_ */
