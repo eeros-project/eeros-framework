006573608e4a43c825d4a4e757b85c4cb2e81db4
Urs Graf <urs.graf@ost.ch>
2026-03-26 15:51:26 +0100

    Remove unnecessary template parameter

--- includes/eeros/control/Mux.hpp
+++ includes/eeros/control/Mux.hpp
@@ -4,22 +4,20 @@
 #include <eeros/control/Blockio.hpp>
 #include <eeros/math/Matrix.hpp>
 
-namespace eeros {
-namespace control {
+namespace eeros::control {
 
 /**
  * A multiplexer block is used to bundle multiple inputs into one output vector.
  *
  * @tparam N - number of inputs
  * @tparam T - input signal data type (double - default type)
- * @tparam C - output signal data type (Matrix<N,1,T> - default type)
  * @tparam U - signal unit type (dimensionless - default type)
  *
  * @since v0.6
  */
 
-template < uint32_t N, typename T = double, typename C = eeros::math::Matrix<N,1,T>, SIUnit U = SIUnit::create() >
-class Mux: public Blockio<N,1,T,C,MakeUnitArray<U,N>::value,MakeUnitArray<U>::value> {
+template < uint32_t N, typename T = double, SIUnit U = SIUnit::create() >
+class Mux: public Blockio<N,1,T,eeros::math::Matrix<N,1,T>,MakeUnitArray<U,N>::value,MakeUnitArray<U>::value> {
  public:
   /**
    * Constructs a multiplexer instance.
@@ -36,7 +34,7 @@ class Mux: public Blockio<N,1,T,C,MakeUnitArray<U,N>::value,MakeUnitArray<U>::va
    *
    */
   void run() override {
-    C newValue;
+    eeros::math::Matrix<N,1,T> newValue;
     for_<N>([&, this]<std::size_t I>() {
       newValue(I) = this->template getIn<I>().getSignal().getValue();
     });
@@ -51,13 +49,12 @@ class Mux: public Blockio<N,1,T,C,MakeUnitArray<U,N>::value,MakeUnitArray<U>::va
  * Multiplexer instance to an output stream.\n
  * Does not print a newline control character.
  */
-template < uint32_t N, typename T, typename C, SIUnit U >
-std::ostream& operator<<(std::ostream& os, const Mux<N, T, C, U>& m) {
+template < uint32_t N, typename T, SIUnit U >
+std::ostream& operator<<(std::ostream& os, const Mux<N, T, U>& m) {
   os << "Block multiplexer: '" << m.getName() << "'"; 
   return os;
 }
 
-}
 }
 
 #endif /* ORG_EEROS_CONTROL_MUX_HPP_ */
